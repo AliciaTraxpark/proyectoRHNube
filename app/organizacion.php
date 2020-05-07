87f7ed4147810c7c63e9577d80c122d6d6872421
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-07 15:02:07 -0500

    organizacion

---
+++ app/organizacion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class organizacion extends Model
+{
+    protected $table = 'organizacion';
+    protected $primaryKey = 'organi_id';
+    protected $fillable = ['organi_id','organi_ruc','organi_razonSocial','organi_direccion',
+                        'organi_departamento','organi_provincia','organi_distrito','organi_nempleados',
+                        'organi_pagWeb','organi_tipo'];
+}
