cb736fc918321d02253e090d6f55e8690a5ce4a3
alicia131 <aliciarodriguez131@gmail.com>
2020-05-07 11:32:42 -0500

    cambio

---
+++ app/persona.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class persona extends Model
+{
+    //
+    protected $table = 'persona';
+    protected $primaryKey = 'perso_id';
+    protected $fillable = ['perso_id','perso_nombre'];
+}
