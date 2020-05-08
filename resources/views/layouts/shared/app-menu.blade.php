5217a46f06468a64504fc66e29ea302e3b3b12a7
alicia131 <aliciarodriguez131@gmail.com>
2020-05-08 14:15:53 -0500

    subido

--- resources/views/layouts/shared/app-menu.blade.php
+++ resources/views/layouts/shared/app-menu.blade.php
@@ -2,7 +2,7 @@
     <li class="menu-title">Navigation</li>
 
     <li>
-        <a href="/">
+        <a href="/sistema">
             <i data-feather="home"></i>
             <span class="badge badge-success float-right">1</span>
             <span> Dashboard </span>
