4c69984b947c3d49b0bd55e4243405fc2139bec2
alicia131 <aliciarodriguez131@gmail.com>
2020-05-08 10:32:50 -0500

    plantilla

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/sistema', function () {
+    return view('Dashboard');
+});
 Auth::routes();
 
 //PERSONA
