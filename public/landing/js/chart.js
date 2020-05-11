cf1fb3c2e86f632cfeff3824f85f87a4536f559b
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-11 17:19:07 -0500

    chart

--- public/landing/js/chart.js
+++ public/landing/js/chart.js
@@ -77,3 +77,43 @@ var options = {
   };
   var chart4 = new ApexCharts(document.querySelector("#chart4"), options4);
   chart4.render();
+  //
+  var options5 = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    type: 'donut',
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+  var chart5 = new ApexCharts(document.querySelector("#chart5"), options5);
+  chart5.render();
+  //
+  var options6 = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    type: 'donut',
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+  var chart6 = new ApexCharts(document.querySelector("#chart6"), options6);
+  chart6.render();
