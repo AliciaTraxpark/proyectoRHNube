2851638fc6f3de9db30a716ebab91452e26591b4
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-11 18:43:34 -0500

    titulo chart

--- public/landing/js/chart.js
+++ public/landing/js/chart.js
@@ -3,6 +3,19 @@ var options = {
     chart: {
     type: 'donut',
   },
+  plotOptions: {
+    pie: {
+      donut: {
+        labels: {
+          show: true,
+          total: {
+            showAlways: true,
+            show: true
+          }
+        }
+      }
+    }
+  },
   responsive: [{
     breakpoint: 480,
     options: {
