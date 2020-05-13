00d62b4c512012782a9a0321df1d2d0c6eef2547
alicia131 <aliciarodriguez131@gmail.com>
2020-05-13 16:43:19 -0500

    dd

--- public/landing/js/calendario.js
+++ public/landing/js/calendario.js
@@ -8,14 +8,14 @@ function calendario() {
     var configuracionCalendario = {
         locale: 'es',
         defaultDate: ano+'-01-01',
-    
+
         plugins: [ 'dayGrid','interaction','timeGrid'],
-       
+
         selectable: true,
         selectMirror: true,
         select: function(arg) {
-    
-    
+
+
          /*  calendar.addEvent({
             title: 'title',
             start: arg.start,
@@ -42,7 +42,7 @@ function calendario() {
           left:'Descanso',
           right:'NoLaborales'
         },
-    
+
         events:"calendario/show",
         customButtons:{
           Descanso:{
@@ -63,7 +63,7 @@ function calendario() {
                 $('#startF').val(start);
                 $('#endF').val(end);
                 $('#myModalFestivo').modal('toggle');
-    
+
             }
           }
         },
@@ -71,98 +71,79 @@ function calendario() {
     var calendar = new FullCalendar.Calendar(calendarEl,configuracionCalendario);
     calendar.setOption('locale',"Es");
      //DESCANSO
-    $('#guardarDescanso').click(function(){
-      objEvento=datos("POST");
-      EnviarDescanso('',objEvento);
-    });
-    $('#eliminarDescanso').click(function(){
-      objEvento=datos("DELETE");
-      EnviarDescansoE('/'+id,objEvento);
-    });
-    function datos(method){
-        nuevoEvento={
-          title: $('#title').val(),
-          color:'#4673a0',
-          textColor:' #ffffff ',
-          start: $('#start').val(),
-          end: $('#end').val(),
-          tipo: 1,
-          '_method':method
-        }
-        return(nuevoEvento);
-    }
-    function EnviarDescanso(accion,objEvento){
-        $.ajax(
-            {
-            type: "POST",
-            url:"/calendario/store" +accion,
-            data:objEvento,
-            headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      },
-            success:function(msg){
-              $('#myModal').modal('toggle');
-              calendar.refetchEvents();
-              console.log(msg); },
-            error:function(){ alert("Hay un error");}
-            }
-        );
-    } 
-    function EnviarDescansoE(accion,objEvento){
-        $.ajax(
-            {
-            type: "DELETE",
-            url:"/calendario" +accion,
-            data:objEvento,
-            headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      },
-            success:function(msg){
-              $('#myModalEliminar').modal('toggle');
-              calendar.refetchEvents();
-              console.log(msg); 
-              },
-            error:function(){ alert("Hay un error");}
-            }
-        );
-    }
-    ///
-    //NO LABORABLE
-    $('#guardarNoLab').click(function(){
-      objEvento1=datos1("POST");
-      EnviarNoL('',objEvento1);
-    });
-    function datos1(method){
-        nuevoEvento1={
-          title: $('#titleN').val(),
-          color:'#a34141',
-          textColor:' #ffffff ',
-          start: $('#startF').val(),
-          end: $('#endF').val(),
-          tipo: 0,
+     $('#guardarDescanso').click(function(){
+        objEvento=datos("POST");
+        EnviarDescanso('',objEvento);
+      });
+      function datos(method){
+          nuevoEvento={
+            title: $('#title').val(),
+            color:'#4673a0',
+            textColor:' #ffffff ',
+            start: $('#start').val(),
+            end: $('#end').val(),
+            tipo: 1,
 
-          '_method':method
-        }
-        return(nuevoEvento1);
-    }
-    function EnviarNoL(accion,objEvento1){
-        $.ajax(
-            {
-            type: "POST",
-            url:"/calendario/store" +accion,
-            data:objEvento1,
-            headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      },
-            success:function(msg){
-              $('#myModalFestivo').modal('toggle');
-              calendar.addEvent(nuevoEvento1);
-              console.log(msg); },
-            error:function(){ alert("Hay un error");}
-            }
-        );
-    }
-    ////
+            '_method':method
+          }
+          return(nuevoEvento);
+
+      }
+      function EnviarDescanso(accion,objEvento){
+          $.ajax(
+              {
+              type: "POST",
+              url:"{{url('/eventos_usuario/store')}}" +accion,
+              data:objEvento,
+              headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+              success:function(msg){
+                $('#myModal').modal('toggle');
+                calendar.addEvent(nuevoEvento);
+                console.log(msg); },
+              error:function(){ alert("Hay un error");}
+              }
+          );
+      } ///
+
+      //NO LABORABLE
+      $('#guardarNoLab').click(function(){
+        objEvento1=datos1("POST");
+        EnviarNoL('',objEvento1);
+      });
+      function datos1(method){
+          nuevoEvento1={
+            title: $('#titleN').val(),
+            color:'#a34141',
+            textColor:' #ffffff ',
+            start: $('#startF').val(),
+            end: $('#endF').val(),
+            tipo: 0,
+
+            '_method':method
+          }
+          return(nuevoEvento1);
+
+      }
+      function EnviarNoL(accion,objEvento1){
+          $.ajax(
+              {
+              type: "POST",
+              url:"{{url('/eventos_usuario/store')}}" +accion,
+              data:objEvento1,
+              headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+              success:function(msg){
+                $('#myModalFestivo').modal('toggle');
+                calendar.addEvent(nuevoEvento1);
+                console.log(msg); },
+              error:function(){ alert("Hay un error");}
+              }
+          );
+      }
+      ////
     calendar.render();
 }
 document.addEventListener('DOMContentLoaded',calendario);
