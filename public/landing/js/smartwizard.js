dd8cb429e96bc6e3a4835061cca92f86280a56bb
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-18 17:17:26 -0500

    url

--- public/landing/js/smartwizard.js
+++ public/landing/js/smartwizard.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('#smartwizard').smartWizard({
+        selected: 0,
+        showStepURLhash: false,
         lang: {  // Language variables
             next: 'Siguiente',
             previous: 'Anterior'
