ce5c64cd5a76335e360ad56064c5846e983252d5
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-19 12:24:07 -0500

    tabla

--- public/landing/js/tabla.js
+++ public/landing/js/tabla.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
         "language" :{
             "search" : "Buscar",
             "info" : "_TOTAL_ empleados",
+            "infoEmpty": "Mostrando 0.",
+            "infoFiltered": "(filtrados de un total de _MAX_ empleados)",
+            "zeroRecords": "No se han encontrado coincidencias.",
             "paginate" : {
                 "next" : "Siguiente",
                 "previous" : "Anterior"
@@ -11,8 +14,15 @@ $(document).ready(function() {
                         '<option value="10">10</option>'+
                         '<option value="15">15</option>'+
                         '<option value="-1">Todos</option>'+
-                        '</select> Empleados'
+                        '</select> Empleados',
+            "select": {
+            "rows": ""
+        }
         },
-        "scrollX" : true 
+        "scrollX" : true ,
+        "select" : true,
+        "tableTools": {
+             "sRowSelect": "single" 
+        }
     });
 } )
