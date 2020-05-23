e0b48268d17470d8d38b9378e1169b4a5c7d1591
alicia131 <aliciarodriguez131@gmail.com>
2020-05-23 13:11:52 -0500

    f

--- resources/views/empleado/empleado.blade.php
+++ resources/views/empleado/empleado.blade.php
@@ -443,7 +443,7 @@
                             <li><a href="#persona-step-1">Personales</a></li>
                             <li><a href="#sw-default-step-2">Empresarial</a></li>
                             <li><a href="#sw-default-step-3">Foto</a></li>
-                            <li><a href=""><button class="btn btn-danger btn-sm">Eliminar</button></a></li>
+                            
                         </ul>
                         <div class="p-3" id="form-registrar">
                             <div id="persona-step-1">
