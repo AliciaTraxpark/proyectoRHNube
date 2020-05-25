4bb3f8ae04725bf8f76f107dd996cf159f993ee1
alicia131 <aliciarodriguez131@gmail.com>
2020-05-25 13:11:41 -0500

    ff

--- resources/views/tareas/tareas.blade.php
+++ resources/views/tareas/tareas.blade.php
@@ -1,12 +1,49 @@
 @extends('layouts.vertical')
-@section('content')
-<div class="content-page">
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row page-title">
-                <h4 class="mb-1 mt-0">Actividad de Captura de Pantalla</h4>
-            </div>
-        </div>
+
+
+@section('css')
+
+@endsection
+
+@section('breadcrumb')
+<div class="row page-title">
+    <div class="col-md-12">
+
+        <h4 class="mb-1 mt-0">Actividad de Captura de Pantalla</h4>
     </div>
 </div>
 @endsection
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-light"><i class="uil uil-arrow-left"></i></button>
+                        <button type="button" class="btn btn-light"><i class="uil uil-arrow-right"></i></button>
+                    </div>
+                    <div class="col-md-1">
+                        
+                    </div>
+
+
+                </div>
+
+
+            </div> <!-- end card-body-->
+        </div> <!-- end card-->
+    </div> <!-- end col-->
+
+
+
+</div>
+<!-- end row -->
+
+
+@endsection
+
+
+
