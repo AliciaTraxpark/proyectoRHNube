501c8c3486ef0d11aa6afd2b875e46d945881e00
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-27 12:29:06 -0500

    validacion exacta

--- public/landing/js/smartwizard.js
+++ public/landing/js/smartwizard.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+    function validateSteps(stepnumber){
+        var isStepValid = true;
+        // validate step 1
+        if(stepnumber == 0){
+            console.log("validar primer paso");
+            if($('#documento').val() == ""||$('#apPaterno').val() == "" || 
+                $('#departamento').val() == "" || $('#dep').val() == "" ||
+                $('#numDocumento').val() == "" || $('#apMaterno').val() == "" ||
+                $('#provincia').val() == "" || $('#prov').val() == "" || $('#fechaN').val() == "" ||
+                $('#nombres').val() == "" || $('#distrito').val() == "" || $('#dist').val() == "" ||
+                $('#direccion').val() == ""){
+                isStepValid = false;
+            }
+            console.log(isStepValid)
+        }
+        if(stepnumber == 1){
+            if($('#contrato').val() == ""){
+                isStepValid = false;
+            }
+        }
+        return isStepValid;    
+    }
     $('#smartwizard').smartWizard({
         selected: 0,
         showStepURLhash: false,
@@ -6,6 +28,10 @@ $(document).ready(function(){
             next: 'Siguiente',
             previous: 'Anterior'
         },
+        leaveStep:function(){
+            alert("aaa");
+            return true;
+        }
     });
     $('#smartwizard1').smartWizard({
         selected: 0,
@@ -15,4 +41,9 @@ $(document).ready(function(){
             previous: 'Anterior'
         },
     });
+    //************Validacion********//
+    $('#smartwizard').on("leaveStep",function leaveAStepCallback(event,obj, indice){
+        console.log(indice);
+        return validateSteps(indice);
+    })         
   });
