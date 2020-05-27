606e7592daca1138ed4a23d0879770af9c34167b
alicia131 <aliciarodriguez131@gmail.com>
2020-05-27 15:46:02 -0500

    ws

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection,WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,18 @@ class UsersExport implements FromCollection
     public function collection()
     {
         return User::all();
+
+    }
+    public function headings(): array
+    {
+        return [
+            '#',
+            'User',
+            'Date',
+            'd',
+            'f',
+            'ff',
+            'fdf',
+        ];
     }
 }
