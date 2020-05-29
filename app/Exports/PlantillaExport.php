e4e621167702c19e07b6dad7d0b88c6ed575571f
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-29 14:56:12 -0500

    tipoDocumento

--- app/Exports/PlantillaExport.php
+++ app/Exports/PlantillaExport.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports;
 
+use App\tipo_contrato;
 use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -15,6 +16,7 @@ use Maatwebsite\Excel\Concerns\WithColumnFormating;
 use App\ubigeo_peru_departments;
 use App\ubigeo_peru_provinces;
 use App\ubigeo_peru_districts;
+use App\tipo_documento;
 
 class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
 {
@@ -45,22 +47,50 @@ class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
                 $styleArray = [
                     'font' => [
                         'bold' => true,
+                    ],
+                    'color'=>[
+                        'rgb' => 'FF0000',
                     ]
                 ];
 
+                $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+                $spreadsheet->getActiveSheet()->getStyle('A1')->applyFromArray($styleArray);
+                $event->sheet->getDelegate()->setTitle("Empleado");
                 $departamentos=ubigeo_peru_departments::all();
                 $provincias = ubigeo_peru_provinces::all();
+                $tipoDocumento = tipo_documento::all();
 
                 $drop_column = 'G';
                 $drop_columnP = 'H';
+                $drop_columnD = 'A';
                 //getHighestRow();
 
                 $row = 1;
+                $rowP = 1;
+                $rowD = 1;
+
+                //TIPODOCUMENTO
+                foreach($tipoDocumento as $tipoDocumentos){
+                    $event->sheet->getDelegate()->setCellValue('CA'.$rowD++,$tipoDocumentos->tipoDoc_id ." ".$tipoDocumentos->tipoDoc_descripcion);
+                }
+
+                $validationD = $event->sheet->getDelegate()->getCell("{$drop_columnD}2")->getDataValidation();
+                $validationD->setType(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::TYPE_LIST);
+                $validationD->setErrorStyle(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::STYLE_INFORMATION);
+                $validationD->setAllowBlank(false);
+                $validationD->setShowInputMessage(true);
+                $validationD->setShowErrorMessage(true);
+                $validationD->setShowDropDown(true);
+                $validationD->setErrorTitle('Input Error');
+                $validationD->setError('Value is not in list.');
+                $validationD->setPromptTitle('Pick from list');
+                $validationD->setPrompt('Please value from');
+                $validationD->setFormula1('Empleado!$CA$1:$CA$3');
+
+                //DEPARTAMENTO
                 foreach($departamentos as $departamento){
                     $event->sheet->getDelegate()->setCellValue('BA'.$row++,$departamento->id." ".$departamento->name);
                 }
-                
-                $event->sheet->getDelegate()->setTitle("Empleado");
 
                 $validation = $event->sheet->getDelegate()->getCell("{$drop_column}2")->getDataValidation();
                 $validation->setType(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::TYPE_LIST);
@@ -75,8 +105,9 @@ class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
                 $validation->setPrompt('Please value from');
                 $validation->setFormula1('Empleado!$BA$1:$BA$25');
 
+                //PROVINCIA
                 foreach($provincias as $provincia){
-                    $event->sheet->getDelegate()->setCellValue('GA'.$row++,$provincia->id." ".$provincia->name);
+                    $event->sheet->getDelegate()->setCellValue('GA'.$rowP++,$provincia->id." ".$provincia->name);
                 }
 
                 $validationP = $event->sheet->getDelegate()->getCell("{$drop_columnP}2")->getDataValidation();
@@ -93,6 +124,7 @@ class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
                 $validationP->setFormula1('Empleado!$GA$1:$GA$25');
 
                 for ($i = 2; $i <= $this->total; $i++) {
+                    $event->sheet->getCell("{$drop_columnD}{$i}")->setDataValidation(clone $validationD);
                     $event->sheet->getCell("{$drop_column}{$i}")->setDataValidation(clone $validation);
                     $event->sheet->getCell("{$drop_columnP}{$i}")->setDataValidation(clone $validationP);
                 }
