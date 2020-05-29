f264c684ecfc55ef2856d6b8e9f4df2b025dbe91
GabrielaRosmeri <gaby020313@gmail.com>
2020-05-29 14:21:21 -0500

    espacio departaemnto

--- app/Exports/PlantillaExport.php
+++ app/Exports/PlantillaExport.php
@@ -13,6 +13,8 @@ use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\NumberFormat;
 use Maatwebsite\Excel\Concerns\WithColumnFormating;
 use App\ubigeo_peru_departments;
+use App\ubigeo_peru_provinces;
+use App\ubigeo_peru_districts;
 
 class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
 {
@@ -47,17 +49,20 @@ class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
                 ];
 
                 $departamentos=ubigeo_peru_departments::all();
+                $provincias = ubigeo_peru_provinces::all();
 
                 $drop_column = 'G';
+                $drop_columnP = 'H';
                 //getHighestRow();
 
                 $row = 1;
                 foreach($departamentos as $departamento){
-                    $event->sheet->getDelegate()->setCellValue('GA'.$row++,$departamento->id."".$departamento->name);
+                    $event->sheet->getDelegate()->setCellValue('BA'.$row++,$departamento->id." ".$departamento->name);
                 }
-                $event->sheet->getDelegate()->setTitle("Departamento");
+                
+                $event->sheet->getDelegate()->setTitle("Empleado");
 
-                $validation = $event->sheet->getDelegate()->getCell("{$drop_column}11")->getDataValidation();
+                $validation = $event->sheet->getDelegate()->getCell("{$drop_column}2")->getDataValidation();
                 $validation->setType(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::TYPE_LIST);
                 $validation->setErrorStyle(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::STYLE_INFORMATION);
                 $validation->setAllowBlank(false);
@@ -68,10 +73,28 @@ class PlantillaExport implements WithHeadings,ShouldAutoSize,WithEvents
                 $validation->setError('Value is not in list.');
                 $validation->setPromptTitle('Pick from list');
                 $validation->setPrompt('Please value from');
-                $validation->setFormula1('Departamento!$GA$1:$GA$25');
+                $validation->setFormula1('Empleado!$BA$1:$BA$25');
+
+                foreach($provincias as $provincia){
+                    $event->sheet->getDelegate()->setCellValue('GA'.$row++,$provincia->id." ".$provincia->name);
+                }
+
+                $validationP = $event->sheet->getDelegate()->getCell("{$drop_columnP}2")->getDataValidation();
+                $validationP->setType(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::TYPE_LIST);
+                $validationP->setErrorStyle(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::STYLE_INFORMATION);
+                $validationP->setAllowBlank(false);
+                $validationP->setShowInputMessage(true);
+                $validationP->setShowErrorMessage(true);
+                $validationP->setShowDropDown(true);
+                $validationP->setErrorTitle('Input Error');
+                $validationP->setError('Value is not in list.');
+                $validationP->setPromptTitle('Pick from list');
+                $validationP->setPrompt('Please value from');
+                $validationP->setFormula1('Empleado!$GA$1:$GA$25');
 
                 for ($i = 2; $i <= $this->total; $i++) {
                     $event->sheet->getCell("{$drop_column}{$i}")->setDataValidation(clone $validation);
+                    $event->sheet->getCell("{$drop_columnP}{$i}")->setDataValidation(clone $validationP);
                 }
             }
         ];
