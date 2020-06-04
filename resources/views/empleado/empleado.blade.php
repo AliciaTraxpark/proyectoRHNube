735ef868cd4a218dc4f13b968e224b5ae9e60626
alicia131 <aliciarodriguez131@gmail.com>
2020-06-03 23:32:09 -0500

    style

--- resources/views/empleado/empleado.blade.php
+++ resources/views/empleado/empleado.blade.php
@@ -599,12 +599,12 @@ nav-item active{
                 <div class="modal fade" id="form-ver" style="font-size: 13px" tabindex="-1" role="dialog" aria-labelledby="areamodal" aria-hidden="true" data-backdrop="static">
                     <div class="modal-dialog modal-dialog-scrollable modal-lg">
                         <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="header-title mt-0 ">Datos de empleado</h4><br>
+                            <div class="modal-header" style="background: #163552;">
+                                <h4 class="header-title mt-0 " style="color: #f0f0f0">Datos de empleado</h4><br>
                             </div>
                             <div class="modal-body" style="padding: 0px;">
                                 <div id="smartwizard1" style="background: #ffffff; color:#3d3d3d;">
-                                    <ul style="background: #566879!important;" >
+                                    <ul style="background: #fdfdfd!important;" >
                                         <li><a href="#persona-step-1">Personales</a></li>
                                         <li><a href="#sw-default-step-2">Empresarial</a></li>
                                         <li><a href="#sw-default-step-3">Foto</a></li>
@@ -622,7 +622,7 @@ nav-item active{
                                                     <input style="display: none;" name="v_id" id="v_id">
                                                     <div class="form-group">
                                                         <label for="sw-default">Tipo Documento</label>
-                                                        <input type="text" class="form-control" name="v_tipoDoc" id="v_tipoDoc" disabled style="background-color: #f0f0f0;">
+                                                        <input type="text" class="form-control" name="v_tipoDoc" id="v_tipoDoc" disabled style="background-color: #fcfcfc;">
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="sw-default">Apellido Paterno</label>
@@ -636,7 +636,7 @@ nav-item active{
                                                 <div class="col-4">
                                                     <div class="form-group">
                                                         <label for="sw-default">Num. Documento</label>
-                                                        <input type="text" class="form-control" name="v_numDocumento" id="v_numDocumento" required disabled style="background-color: #f0f0f0;">
+                                                        <input type="text" class="form-control" name="v_numDocumento" id="v_numDocumento" required disabled style="background-color: #fcfcfc;">
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="sw-default">Apellido Materno</label>
