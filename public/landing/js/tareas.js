3232d25788abef77920ba484e82d39c0239246e4
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-05 09:31:26 -0500

    tareas con estilos

--- public/landing/js/tareas.js
+++ public/landing/js/tareas.js
@@ -15,15 +15,35 @@ function onMostrarPantallas(){
             var container = $('#card');
             console.log(data)
             for(var i=0; i<data.length; i++){
-                //label += '<label>'+data[i].hora_i+' '+data[i].hora_f+'</label>'
-                //image += '<img src="data:image/jpeg;base64,'+data[i].Img+'">';
-                //$('#hora').html(label);
-                //$('#imagen').html(image);
                 card = `<div class="card-body" style="padding-left: 0px;">
                         <label>${data[i].hora_i} ${data[i].hora_f}</label>
-                        <div class="row">
-                        <img src="data:image/jpeg;base64,${data[i].Imag}" height="150" width="150">
-                    </div>
+                            <div class="custom-accordion accordion ml-4" id="customaccordion_exa" style="margin-left: 0px!important;">
+                                    <div class="card mb-1" style="padding-left: 20px;">
+                                        <a href="" class="text-dark" data-toggle="collapse" data-target="#customaccorcollapseOne"
+                                            aria-expanded="true" aria-controls="customaccorcollapseOne">
+                                        </a>
+                                        <div id="customaccorcollapseOne" class="collapse show" aria-labelledby="customaccorheadingOne"
+                                            data-parent="#customaccordion_exa">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <div class=" text-center col-md-12 col-sm-6" style="background-color: rgb(234, 234, 234);padding-top: 8px;
+                                                padding-bottom: 8px;">
+                                                    <h5 class="m-0 font-size-14" > Área 1  </h5>
+                                                </div>  <br>
+                                                <div class="col-md-12 col-sm-6 border" style="padding-left: 0px;">
+                                                <img src="data:image/jpeg;base64,${data[i].Imag}" height="225" width="225">
+                                                &nbsp;  <label style="font-size: 12px" for="">9:00 am - 9:10 am</label>
+                                                <div class="progress" style="background-color: #d4d4d4;">
+                                                    &nbsp;  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                                                        aria-valuemin="0" aria-valuemax="100">25%</div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                     </div>`
             }
             container.append(card);
