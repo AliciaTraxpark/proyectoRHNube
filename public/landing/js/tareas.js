aeb77ea1292d86c3b593a20c01d346663ce5cda1
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-06 12:14:40 -0500

    modficando estilos

--- public/landing/js/tareas.js
+++ public/landing/js/tareas.js
@@ -17,55 +17,52 @@ function onMostrarPantallas(){
             var labelDelGrupo = horaDelGrupo+":00:00" + " - " + (horaDelGrupo+1) + ":00:00";
             var grupo = `<div class="card">${labelDelGrupo}`
             console.log(data);
-            for(var i=0; i<data.length; i++){
+            for(var i=0; i<6; i++){
                 console.log(horaDelGrupo);
                 console.log(data[i].hora_ini);
-                if(parseInt(data[i].hora_ini.split(":")[0]) > horaDelGrupo){
-                    grupo += `</div><br>`;
-                    container.append(grupo);
-                    console.log(grupo);
-                    horaDelGrupo = parseInt(data[i].hora_ini.split(":")[0]);
-                    var labelDelGrupo = horaDelGrupo + ":00:00" + (horaDelGrupo+1) + ":00:00";
-                    grupo = `<div class="card">${labelDelGrupo}`;
-                }
-                card = `<div class="card-body" style="padding-left: 0px;">
-                            <div class="custom-accordion accordion ml-4" id="customaccordion_exa" style="margin-left: 0px!important;">
-                                    <div class="card mb-1" style="padding-left: 20px;">
-                                        <a href="" class="text-dark" data-toggle="collapse" data-target="#customaccorcollapseOne"
-                                            aria-expanded="true" aria-controls="customaccorcollapseOne">
-                                        </a>
-                                        <div id="customaccorcollapseOne" class="collapse show" aria-labelledby="customaccorheadingOne"
-                                            data-parent="#customaccordion_exa">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class=" text-center col-md-12 col-sm-6" style="background-color: rgb(234, 234, 234);padding-top: 8px;
-                                                padding-bottom: 8px;">
-                                                    <h5 class="m-0 font-size-14" >${data[i].Proye_Nombre} </h5>
-                                                </div>  <br>
-                                                <div class="col-md-12 col-sm-6 border" style="padding-left: 0px;">
-                                                <img src="data:image/jpeg;base64,${data[i].imagen}" height="225" width="225">
-                                                &nbsp;  <label style="font-size: 12px" for="">9:00 am - 9:10 am</label>
-                                                <div class="progress" style="background-color: #d4d4d4;">
-                                                    &nbsp;  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                                        aria-valuemin="0" aria-valuemax="100">25%</div>
-                                                </div>
+                for(var j=0; j>6; j++){
+                    if(parseInt(data[i].hora_ini.split(":")[1].charAt(0)) == j){
+                        card = `<div class="card-body" style="padding-left: 0px;">
+                                <div class="custom-accordion accordion ml-4" id="customaccordion_exa" style="margin-left: 0px!important;">
+                                        <div class="card mb-1" style="padding-left: 20px;">
+                                            <a href="" class="text-dark" data-toggle="collapse" data-target="#customaccorcollapseOne"
+                                                aria-expanded="true" aria-controls="customaccorcollapseOne">
+                                            </a>
+                                            <div id="customaccorcollapseOne" class="collapse show" aria-labelledby="customaccorheadingOne"
+                                                data-parent="#customaccordion_exa">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class=" text-center col-md-12 col-sm-6" style="background-color: rgb(234, 234, 234);padding-top: 8px;
+                                                    padding-bottom: 8px;">
+                                                        <h5 class="m-0 font-size-14" >${data[i].Proye_Nombre} </h5>
+                                                    </div>  <br>
+                                                    <div class="col-md-12 col-sm-6 border" style="padding-left: 0px;">
+                                                    <img src="data:image/jpeg;base64,${data[i].imagen}" height="225" width="225">
+                                                    &nbsp;  <label style="font-size: 12px" for="">9:00 am - 9:10 am</label>
+                                                    <div class="progress" style="background-color: #d4d4d4;">
+                                                        &nbsp;  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                                                            aria-valuemin="0" aria-valuemax="100">25%</div>
+                                                    </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                    </div>`
-                    grupo +=card;
-
-                    if(i==data.length-1){
-                        grupo += `</div><br>`;
-                        container.append(grupo);
-                        horaDelGrupo = parseInt(data[i].hora_ini.split(":")[0]);
-                        var labelDelGrupo = horaDelGrupo + ":00:00" + (horaDelGrupo+1) + ":00:00";
-                        grupo = `<div class="card">${labelDelGrupo}`;
+                        </div>`
+                        i++;
+                    }else{
+                        card = `<div class="card-body">Vacio</div>`
                     }
+                    grupo += card;
+                }
+                grupo += `</div><br>`;
+                container.append(grupo);
+                console.log(grupo);
+                horaDelGrupo = parseInt(data[i].hora_ini.split(":")[0]);
+                var labelDelGrupo = horaDelGrupo + ":00:00" + (horaDelGrupo+1) + ":00:00";
+                grupo = `<div class="card">${labelDelGrupo}`;
             }
         },
         error:function(data){
