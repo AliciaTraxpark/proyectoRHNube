27cb0783d3ec8316ac47045b077b646aca2a556b
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-08 12:44:55 -0500

    ya funciona el subir foto

--- app/Http/Controllers/CargaMasivaFotoController.php
+++ app/Http/Controllers/CargaMasivaFotoController.php
@@ -11,10 +11,15 @@ class CargaMasivaFotoController extends Controller
     public function subirfoto(Request $request){
         $file = $request->file('fileMasiva');
         $idempleado=explode(".",$file[0]->getClientOriginalName());
-        $empleado= DB::table('empleado as e')
-            ->where('e.emple_nDoc','=',$idempleado[0])
-            ->get();
-        $empleado->emple_foto=$idempleado;
-        return json_encode(array('status'=>true));
+        $empleado = empleado::where('emple_nDoc',$idempleado)->first();
+        if($empleado){
+            $path = public_path() . '/fotosEmpleado';
+            $fileName = uniqid().$file[0]->getClientOriginalName();
+            $file[0]->move($path,$fileName);
+            $empleado->emple_foto=$fileName;
+            $empleado->save();
+            return json_encode(array('status'=>true));
+        }
+        return json_encode(array('status'=>false));
     }
 }
