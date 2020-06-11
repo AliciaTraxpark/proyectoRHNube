e188632cce7e188c21f56499ad00001421023edd
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-10 21:31:44 -0500

    script chart

--- resources/views/tareas/reporteSemanal.blade.php
+++ resources/views/tareas/reporteSemanal.blade.php
@@ -2,10 +2,9 @@
 
 @section('css')
 <link href="{{ URL::asset('admin/assets/libs/flatpickr/flatpickr.min.css') }}" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
-<script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <link href="{{ URL::asset('admin/assets/libs/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('admin/assets/libs/chart/Chart.min.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('breadcrumb')
@@ -108,94 +107,10 @@
 <script src="{{ URL::asset('admin/assets/libs/datatables/datatables.min.js') }}"></script>
 <script src="{{ URL::asset('admin/assets/libs/flatpickr/flatpickr.min.js') }}"></script>
 <script src="{{ URL::asset('admin/assets/libs/flatpickr/es.js') }}"></script>
+<script src="{{ URL::asset('admin/assets/libs/chart/Chart.min.js') }}"></script>
 <script src="{{asset('landing/js/tablaReporte.js')}}"></script>
 <script src="{{ URL::asset('admin/assets/js/pages/datatables.init.js') }}"></script>
 <script src="{{asset('admin/assets/libs/combodate-1.0.7/moment.js')}}" ></script>
 <script src="{{asset('admin/assets/libs/combodate-1.0.7/es.js')}}" ></script>
 <script src="{{asset('landing/js/reporteS.js')}}"></script>
-<script>
-    const dataSource = {
-  chart: {
-    caption: "Horas trabajadas por Miembro",
-    theme: "fusion"
-  },
-  data: [
-    {
-      label: "Venezuela",
-      value: "290"
-    },
-    {
-      label: "Saudi",
-      value: "260"
-    },
-    {
-      label: "Canada",
-      value: "230"
-    },
-    {
-      label: "Iran",
-      value: "200"
-    },
-    {
-      label: "Russia",
-      value: "170"
-    },
-    {
-      label: "UAE",
-      value: "140"
-    },
-    {
-      label: "US",
-      value: "110"
-    },
-    {
-      label: "China",
-      value: "80"
-    },
-    {
-      label: "Venezuela",
-      value: "90"
-    },
-    {
-      label: "Saudi",
-      value: "60"
-    },
-    {
-      label: "Canada",
-      value: "80"
-    },
-    {
-      label: "Iran",
-      value: "140"
-    },
-    {
-      label: "Russia",
-      value: "115"
-    },
-    {
-      label: "UAE",
-      value: "100"
-    },
-    {
-      label: "US",
-      value: "30"
-    },
-    {
-      label: "China",
-      value: "30"
-    }
-  ]
-};
-
-FusionCharts.ready(function() {
-  var myChart = new FusionCharts({
-    type: "column2d",
-    renderAt: "chart-container",
-    width: "100%",
-    height: "450%",
-    dataFormat: "json",
-    dataSource
-  }).render();
-});
-</script>
 @endsection
