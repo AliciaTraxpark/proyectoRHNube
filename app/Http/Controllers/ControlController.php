72794a26627166fdab72bd0c9b3893fafd3baf0e
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-11 14:43:05 -0500

    ordenando

--- app/Http/Controllers/ControlController.php
+++ app/Http/Controllers/ControlController.php
@@ -80,34 +80,6 @@ class ControlController extends Controller
         return response()->json($respuesta,200);
     }
 
-    public function store(Request $request){
-        $envio = new envio();
-        $envio->hora_Envio=$request->get('hora_Envio');
-        $envio->Total_Envio=$request->get('Total_Envio');
-        $envio->idEmpleado=$request->get('idEmpleado');
-        $envio->save();
-        $idEnvio=$envio->idEnvio;
-
-        $captura = new captura();
-        $captura->idEnvio=$idEnvio;
-        $captura->estado=$request->get('estado');
-        $captura->fecha_hora=$request->get('fecha_hora');
-        $captura->imagen=$request->get('imagen');
-        $captura->save();
-
-        $control = new control();
-        $control->Proyecto_Proye_id=$request->get('Proyecto_Proye_id');
-        $control->fecha_ini=$request->get('fecha_ini');
-        $control->Fecha_fin=$request->get('Fecha_fin');
-        $control->hora_ini=$request->get('hora_ini');
-        $control->hora_fin=$request->get('hora_fin');
-        $control->idEnvio=$idEnvio;
-        $control->save();
-
-        return response()->json($control,200);
-
-    }
-
     public function proyecto(Request $request){
         $idempleado=$request->get('value');
         $proyecto = DB::table('empleado as e')
