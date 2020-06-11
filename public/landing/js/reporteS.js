235323b0dfe787bfb66b14e9815b9e3b27331c42
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-10 23:05:18 -0500

    falta mostrar datos en el grafico

--- public/landing/js/reporteS.js
+++ public/landing/js/reporteS.js
@@ -22,10 +22,12 @@ function onSelectFechas(){
         success:function(data){
             var container = $('#empleado');
             var containerD = $('#dias');
-            var dias =[];
+            var nombre =[];
+            var horas=[];
+            var color = ['rgb(255, 99, 132)'];
+            var borderColor = ['rgb(255, 99, 132)'];
             var html_tr = "";
             var html_trD = "<tr><th><img src='admin/assets/images/users/empleado.png' class='mr-2' alt='' />Miembro</th>";
-            console.log(data);
             for(var i=0; i<data.length; i++){
                 html_tr += '<tr><td>'+ data[i].nombre + ' ' + data[i].apPaterno + ' ' + data[i].apMaterno + '</td>';
                 for(let j = 0; j < data[i].horas.length; j++){
@@ -33,6 +35,8 @@ function onSelectFechas(){
                         html_tr += '<td>No Trabajo</td>';
                     }else{
                         html_tr += '<td>'+ data[i].horas[j] + '</td>';
+                        horas.push(data[i].horas[j]);
+                        console.log(horas);
                     }
                 }
                 html_tr += '</tr>';
@@ -42,33 +46,34 @@ function onSelectFechas(){
                     momentValue.toDate();
                     momentValue.format("ddd");
                     html_trD += '<th>'+momentValue.format("ddd")+'</th>';
-                    dias[m] += momentValue.format("ddd");
-                    console.log(momentValue.format("ddd"));
+                    nombre.push(momentValue.format("ddd"));
             }
             html_trD += '</tr>';
             container.append(html_tr);
             containerD.append(html_trD);
+
+            var chartdata = {
+                labels: nombre,
+                datasets: [{
+                    label: nombre,
+                    backgroundColor: color,
+                    borderColor: color,
+                    borderWidth: 2,
+                    hoverBackgroundColor: color,
+                    hoverBorderColor: borderColor,
+                    data:horas
+                }]
+            };
+            var mostrar = $("#myChart");
+            var grafico = new Chart(mostrar, {
+                type: 'bar',
+                data: chartdata,
+                options: {
+                    responsive: true,
+                }
+            });
         },
         error:function(data){
         }
     })
 }
-var ctx = $('#myChart');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
