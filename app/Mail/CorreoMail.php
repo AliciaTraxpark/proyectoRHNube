11a88e165e39fb7958979c24ac805a19f71499a1
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-26 10:07:46 -0500

    colas de correo

--- app/Mail/CorreoMail.php
+++ app/Mail/CorreoMail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class CorreoMail extends Mailable
+class CorreoMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
