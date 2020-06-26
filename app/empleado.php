c83fd84c2aa18de42ac9a1d21d4c1cfd2017a44e
alicia131 <aliciarodriguez131@gmail.com>
2020-06-26 18:09:13 -0500

    excel ok

--- app/empleado.php
+++ app/empleado.php
@@ -12,5 +12,5 @@ class empleado extends Model
     protected $fillable = ['emple_id','emple_tipoDoc','emple_nDoc',
     'emple_persona','emple_departamento','emple_provincia','emple_distrito','emple_cargo','emple_area',
         'emple_centCosto','emple_departamentoN','emple_provinciaN',	'emple_distritoN','emple_tipoContrato',
-        'emple_local','emple_nivel','emple_foto','users_id','emple_pasword'];
+        'emple_local','emple_estado','emple_nivel','emple_foto','users_id','emple_pasword'];
 }
