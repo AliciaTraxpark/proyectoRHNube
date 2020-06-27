22eab72ba7745575bbf9512ec28762b808a91bd2
GabrielaRosmeri <gaby020313@gmail.com>
2020-06-27 09:50:13 -0500

    cambiando url

--- resources/views/mails/confirmation_code.blade.php
+++ resources/views/mails/confirmation_code.blade.php
@@ -51,7 +51,7 @@
                 </div>
                 <div>
                     <div class="col-md-12"> <br><br>
-                        <a href="{{'http://3.208.88.131:8000/register/verify/' . $datos->confirmation_code}}"><button
+                        <a href="{{url('register/verify/' . $datos->confirmation_code)}}"><button
                                 class="btn btn-opacity-primary mr-1">
                                 VERIFICA TU CORREO ELECTRONICO </button></a>
                     </div>
