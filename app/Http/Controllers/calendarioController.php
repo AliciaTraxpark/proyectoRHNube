0bed8767a05c14a5c844d4b305646b502d383f66
GabrielaRosmeri <gaby020313@gmail.com>
2020-07-10 14:09:19 -0500

    falta validadr cosas

--- app/Http/Controllers/calendarioController.php
+++ app/Http/Controllers/calendarioController.php
@@ -17,7 +17,7 @@ class calendarioController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth','verified']);
     }
     //
     public function index()
