ffc165bb0fd5edb0e606e6649a07330a9f745933
GabrielaRosmeri <gaby020313@gmail.com>
2020-07-10 21:26:32 -0500

    alertas en carga masiva de fotos

--- public/landing/js/cargaMasivaF.js
+++ public/landing/js/cargaMasivaF.js
@@ -8,6 +8,9 @@ $('#cerrarIF').click('change', function () {
     $("#fileMasiva").fileinput('refresh');
 });
 $(document).ready(function () {
+    var correcto;
+    var quitarIndividual;
+    var quitarTodo;
     $("#fileMasiva").fileinput({
         browseLabel: 'Seleccionar',
         allowedFileExtensions: ['jpg', 'jpeg', 'png'],
@@ -22,11 +25,31 @@ $(document).ready(function () {
         showBrowse: true,
         browseOnZoneClick: true,
         theme: "fa",
+        removeFromPreviewOnErrror: true,
         uploadExtraData: function () {
             return {
                 _token: $("input[name='_token']").val(),
             };
         }
+    }).on('fileloaded', function (event, index, data, msg) {
+        correcto = $.notify({
+            message: data,
+            icon: 'admin/images/checked.svg'
+        }, {
+            element: $('#modalMasivaFoto'),
+            position: 'fixed',
+            icon_type: 'image',
+            allow_dismiss: true,
+            newest_on_top: true,
+            delay: 6000,
+            template: '<div data-notify="container" class="col-xs-12 col-sm-3 text-center alert" style="background-color: #dff0d8;" role="alert">' +
+                '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                '<span data-notify="title">{1}</span> ' +
+                '<span style="color:#3c763d;" data-notify="message">{2}</span>' +
+                '</div>',
+            spacing: 35
+        });
     }).on('fileuploaded', function (event, index, data, msg) {
         $.notify({
             message: "\nFoto registrada\n" + data,
@@ -46,10 +69,49 @@ $(document).ready(function () {
                 '</div>',
             spacing: 35
         });
-    }).on('fileuploaderror', function (event, index, data, msg) {
-        console.log(data);
+    }).on('fileremoved', function (event, id, index) {
         $.notify({
-            message: 'Error:\n' + data,
+            message: 'Retiro la imagen\n' + index + '\n de la collección.',
+            icon: 'admin/images/warning.svg'
+        }, {
+            element: $('#modalMasivaFoto'),
+            position: 'fixed',
+            icon_type: 'image',
+            allow_dismiss: true,
+            newest_on_top: true,
+            delay: 6000,
+            template: '<div data-notify="container" class="col-xs-12 col-sm-3 text-center alert" style="background-color: #fcf8e3;" role="alert">' +
+                '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                '<span data-notify="title">{1}</span> ' +
+                '<span style="color:#8a6d3b;" data-notify="message">{2}</span>' +
+                '</div>',
+            spacing: 35
+        });
+    }).on('fileclear', function (event) {
+        correcto = $.notifyClose();
+        $.notify({
+            message: 'Retiro todas las imagenes de la collección.',
+            icon: 'admin/images/warning.svg'
+        }, {
+            element: $('#modalMasivaFoto'),
+            position: 'fixed',
+            icon_type: 'image',
+            allow_dismiss: true,
+            newest_on_top: true,
+            delay: 6000,
+            template: '<div data-notify="container" class="col-xs-12 col-sm-3 text-center alert" style="background-color: #fcf8e3;" role="alert">' +
+                '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                '<span data-notify="title">{1}</span> ' +
+                '<span style="color:#8a6d3b;" data-notify="message">{2}</span>' +
+                '</div>',
+            spacing: 35
+        });
+    }).on('fileuploaderror', function (event, data, msg) {
+        var msgN = msg.split("Bad Request");
+        $.notify({
+            message: 'Error:\n' + data.index + data.fileId + msgN,
             icon: 'landing/images/warning (1).svg',
         }, {
             element: $('#modalMasivaFoto'),
