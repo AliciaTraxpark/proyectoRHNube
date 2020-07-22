47be0d0252e606a5427fb5636c6fe4badc3a432a
GabrielaRosmeri <gaby020313@gmail.com>
2020-07-21 20:21:53 -0500

    dowload

--- app/Http/Controllers/downloadController.php
+++ app/Http/Controllers/downloadController.php
@@ -31,7 +31,7 @@ class downloadController extends Controller
             $vinculacion->descarga = null;
             $vinculacion->fecha_descarga = Carbon::now();
             $vinculacion->save();
-            return response()->download(app_path() . "/file/Debug.rar");
+            return response()->download(app_path() . "/file/NUBERH.msi");
         }
         return view('Verificacion.link');
     }
