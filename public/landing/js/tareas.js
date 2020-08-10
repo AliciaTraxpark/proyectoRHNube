e41f221b992455d7cecf6172a870bc9905f72803
GabrielaRosmeri <gaby020313@gmail.com>
2020-08-10 06:55:53 -0500

    este si

--- public/landing/js/tareas.js
+++ public/landing/js/tareas.js
@@ -153,7 +153,7 @@ function onMostrarPantallas() {
                                                     </div>
                                                     &nbsp;
                                                     <label style="font-size: 12px" for="">${hora + ":" + (j-1) + "0" + " - " + hora + 
-                                                    ":" + (j) + "9"}</label>
+                                                    ":" + (j-1) + "9"}</label>
                                                     <div class="progress" style="background-color: #d4d4d4;" data-toggle="tooltip" data-placement="bottom" title="Actividad por Rango de Tiempo"
                                                     data-original-title="">
                                                         <div class="progress-bar" role="progressbar" style="width:${promedio}%;background:${nivel}" aria-valuenow=${promedio}
@@ -203,7 +203,7 @@ function onMostrarPantallas() {
                                                     </div>
                                                         &nbsp;
                                                         <label style="font-size: 12px" for="">${hora + ":" + (j-1) + "0" + " - " + hora + 
-                                                        ":" + j + "9"}</label>
+                                                        ":" + (j-1) + "9"}</label>
                                                         <div class="progress" style="background-color: #d4d4d4;" data-toggle="tooltip" data-placement="bottom" title="Actividad por Rango de Tiempo"
                                                         data-original-title="">
                                                             <div class="progress-bar" role="progressbar" style="width:${promedio}%;background:${nivel}" aria-valuenow=${promedio}
