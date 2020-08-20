e34bd810aceb9969f2fdd9c98c3317ea2a0f3947
GabrielaRosmeri <gaby020313@gmail.com>
2020-08-20 16:08:06 -0500

    modelo condicion

---
+++ app/condicion_pago.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class condicion_pago extends Model
+{
+    protected $table = 'condicion_pago';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id', 
+        'condicion', 
+        'user_id'
+    ];
+    public $timestamps = false;
+}
