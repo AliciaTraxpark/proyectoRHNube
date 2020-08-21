93c7834ea4ca213a00045fe1c5fa3e396b42031a
GabrielaRosmeri <gaby020313@gmail.com>
2020-08-21 16:52:07 -0500

    mejorando algunas cosad

--- public/landing/js/reporteS.js
+++ public/landing/js/reporteS.js
@@ -6,6 +6,18 @@ $('#fecha').datetimepicker({
     autoclose: true
 });
 
+var notify = $.notifyDefaults({
+    icon_type: 'image',
+    newest_on_top: true,
+    delay: 4000,
+    template: '<div data-notify="container" class="col-xs-12 col-sm-3 text-center alert" style="background-color: #fcf8e3;" role="alert">' +
+        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+        '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span style="color:#8a6d3b" data-notify="message">{2}</span>' +
+        '</div>'
+});
+
 function acumular60(suma, acumulado) {
     if (suma > 60) {
         suma = suma - 60;
@@ -99,150 +111,153 @@ function onSelectFechas() {
             }*/
         },
         success: function (data) {
-            //var container = $('#empleado');
-            //var containerD = $('#dias');
-            console.log(data);
-            $('#myChartD').hide();
-            var nombre = [];
-            var horas = [];
-            var prom = [];
-            var color = ['rgb(63,77,113)'];
-            var borderColor = ['rgb(63,77,113)'];
-            var html_tr = "";
-            var html_trD = "<tr><th><img src='admin/assets/images/users/empleado.png' class='mr-2' alt='' />Miembro</th>";
-            for (var i = 0; i < data.length; i++) {
-                html_tr += '<tr><td>' + data[i].nombre + ' ' + data[i].apPaterno + ' ' + data[i].apMaterno + '</td>';
-                nombre.push(data[i].nombre.split('')[0] + data[i].apPaterno.split('')[0] + data[i].apMaterno.split('')[0]);
-                var total = data[i].horas.reduce(function (a, b) {
-                    return sumarHora(a, b);
-                });
-                /*var promedio = data[i].promedio.reduce(function (a, b) {
-                    return sumarHora(a, b);
-                });*/
-                var promedio = data[i].promedio.reduce(function (a, b) {
-                    return calcularPromedio(a, b);
-                });
-                console.log(data[i].total);
-                var contar = data[i].total.reduce(function (a, b) {
-                    return totalContar(a, b);
-                });
-                for (let j = 0; j < data[i].horas.length; j++) {
-                    html_tr += '<td>' + data[i].horas[j] + '</td>';
+            if (data.length > 0) {
+                $('#myChartD').hide();
+                var nombre = [];
+                var horas = [];
+                var prom = [];
+                var color = ['rgb(63,77,113)'];
+                var borderColor = ['rgb(63,77,113)'];
+                var html_tr = "";
+                var html_trD = "<tr><th><img src='admin/assets/images/users/empleado.png' class='mr-2' alt='' />Miembro</th>";
+                for (var i = 0; i < data.length; i++) {
+                    html_tr += '<tr><td>' + data[i].nombre + ' ' + data[i].apPaterno + ' ' + data[i].apMaterno + '</td>';
+                    nombre.push(data[i].nombre.split('')[0] + data[i].apPaterno.split('')[0] + data[i].apMaterno.split('')[0]);
+                    var total = data[i].horas.reduce(function (a, b) {
+                        return sumarHora(a, b);
+                    });
+                    /*var promedio = data[i].promedio.reduce(function (a, b) {
+                        return sumarHora(a, b);
+                    });*/
+                    var promedio = data[i].promedio.reduce(function (a, b) {
+                        return calcularPromedio(a, b);
+                    });
+                    console.log(data[i].total);
+                    var contar = data[i].total.reduce(function (a, b) {
+                        return totalContar(a, b);
+                    });
+                    for (let j = 0; j < data[i].horas.length; j++) {
+                        html_tr += '<td>' + data[i].horas[j] + '</td>';
+                    }
+                    var p1 = (promedio[0] / contar[0]).toFixed(2);
+                    var sumaP = p1;
+                    /*var t1 = total.split(":");
+                    var sumaT = parseInt(t1[0]) * 3600 + parseInt(t1[1]) * 60 + parseInt(t1[2]);*/
+                    /*var sumaTotalP = 0;
+                    if (sumaT != 0) {
+                        sumaTotalP = Math.round((sumaP * 100) / sumaT);
+                        prom.push(sumaTotalP);
+                    } else {
+                        sumaTotalP = 0;
+                        prom.push(sumaTotalP);
+                    }*/
+                    html_tr += '<td>' + total + '</td>';
+                    html_tr += '<td>' + sumaP + '%' + '</td>';
+                    var decimal = parseFloat(total.split(":")[0] + "." + total.split(":")[1] + total.split(":")[2]);
+                    horas.push(decimal);
+                    html_tr += '</tr>';
                 }
-                var p1 = (promedio[0] / contar[0]).toFixed(2);
-                var sumaP = p1;
-                /*var t1 = total.split(":");
-                var sumaT = parseInt(t1[0]) * 3600 + parseInt(t1[1]) * 60 + parseInt(t1[2]);*/
-                /*var sumaTotalP = 0;
-                if (sumaT != 0) {
-                    sumaTotalP = Math.round((sumaP * 100) / sumaT);
-                    prom.push(sumaTotalP);
-                } else {
-                    sumaTotalP = 0;
-                    prom.push(sumaTotalP);
-                }*/
-                html_tr += '<td>' + total + '</td>';
-                html_tr += '<td>' + sumaP + '%' + '</td>';
-                var decimal = parseFloat(total.split(":")[0] + "." + total.split(":")[1] + total.split(":")[2]);
-                horas.push(decimal);
-                html_tr += '</tr>';
-            }
-            console.log(data[0].fechaF);
-            for (var m = 0; m < data[0].fechaF.length; m++) {
-                var momentValue = moment(data[0].fechaF[m]);
-                momentValue.toDate();
-                momentValue.format("ddd DD/MM");
-                html_trD += '<th>' + momentValue.format("ddd DD/MM") + '</th>';
-            }
-            html_trD += '<th>TOTAL</th>';
-            html_trD += '<th>ACTIV.</th></tr>';
-            $("#dias").html(html_trD);
-            $("#empleado").html(html_tr);
-            //container.append(html_tr);
-            //containerD.append(html_trD);
+                for (var m = 0; m < data[0].fechaF.length; m++) {
+                    var momentValue = moment(data[0].fechaF[m]);
+                    momentValue.toDate();
+                    momentValue.format("ddd DD/MM");
+                    html_trD += '<th>' + momentValue.format("ddd DD/MM") + '</th>';
+                }
+                html_trD += '<th>TOTAL</th>';
+                html_trD += '<th>ACTIV.</th></tr>';
+                $("#dias").html(html_trD);
+                $("#empleado").html(html_tr);
+                //container.append(html_tr);
+                //containerD.append(html_trD);
 
-            $("#Reporte").DataTable({
-                "searching": true,
-                "scrollX": true,
-                retrieve: true,
-                "ordering": false,
-                language: {
-                    "sProcessing": "Procesando...",
-                    "sLengthMenu": "Mostrar _MENU_ registros",
-                    "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en esta tabla",
-                    "sInfo": "Mostrando registros del _START_ al _END_ ",
-                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                    "sInfoPostFix": "",
-                    "sSearch": "Buscar:",
-                    "sUrl": "",
-                    "sInfoThousands": ",",
-                    "sLoadingRecords": "Cargando...",
-                    "oPaginate": {
-                        "sFirst": "Primero",
-                        "sLast": "Último",
-                        "sNext": ">",
-                        "sPrevious": "<"
+                $("#Reporte").DataTable({
+                    "searching": true,
+                    "scrollX": true,
+                    retrieve: true,
+                    "ordering": false,
+                    language: {
+                        "sProcessing": "Procesando...",
+                        "sLengthMenu": "Mostrar _MENU_ registros",
+                        "sZeroRecords": "No se encontraron resultados",
+                        "sEmptyTable": "Ningún dato disponible en esta tabla",
+                        "sInfo": "Mostrando registros del _START_ al _END_ ",
+                        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                        "sInfoPostFix": "",
+                        "sSearch": "Buscar:",
+                        "sUrl": "",
+                        "sInfoThousands": ",",
+                        "sLoadingRecords": "Cargando...",
+                        "oPaginate": {
+                            "sFirst": "Primero",
+                            "sLast": "Último",
+                            "sNext": ">",
+                            "sPrevious": "<"
+                        },
+                        "oAria": {
+                            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                        },
+                        "buttons": {
+                            "copy": "Copiar",
+                            "colvis": "Visibilidad"
+                        }
                     },
-                    "oAria": {
-                        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                    },
-                    "buttons": {
-                        "copy": "Copiar",
-                        "colvis": "Visibilidad"
+                    dom: 'Bfrtip',
+                    buttons: [{
+                        extend: 'excel',
+                        text: "<i><img src='admin/images/excel.svg' height='20'></i> Descargar",
+                        customize: function (xlsx) {
+                            var sheet = xlsx.xl.worksheets['sheet1.xml'];
+                        },
+                        sheetName: 'Exported data',
+                        autoFilter: false
+                    }, {
+                        extend: "pdfHtml5",
+                        text: "<i><img src='admin/images/pdf.svg' height='20'></i> Descargar",
+                        pageSize: 'LEGAL',
+                        title: 'RH SOLUTION REPORTE SEMANAL'
+                    }],
+                    paging: true
+                });
+                var chartdata = {
+                    labels: nombre,
+                    datasets: [{
+                        label: nombre,
+                        backgroundColor: color,
+                        borderColor: color,
+                        borderWidth: 2,
+                        hoverBackgroundColor: color,
+                        hoverBorderColor: borderColor,
+                        data: horas
+                    }]
+                };
+                var mostrar = $("#myChart");
+                grafico = new Chart(mostrar, {
+                    type: 'bar',
+                    data: chartdata,
+                    theme: "light2",
+                    options: {
+                        responsive: true,
+                        scales: {
+                            xAxes: [{
+                                stacked: true,
+                                gridLines: {
+                                    display: false
+                                }
+                            }],
+                            yAxes: [{
+                                stacked: true
+                            }]
+                        }
                     }
-                },
-                dom: 'Bfrtip',
-                buttons: [{
-                    extend: 'excel',
-                    text: "<i><img src='admin/images/excel.svg' height='20'></i> Descargar",
-                    customize: function (xlsx) {
-                        var sheet = xlsx.xl.worksheets['sheet1.xml'];
-                    },
-                    sheetName: 'Exported data',
-                    autoFilter: false
-                }, {
-                    extend: "pdfHtml5",
-                    text: "<i><img src='admin/images/pdf.svg' height='20'></i> Descargar",
-                    pageSize: 'LEGAL',
-                    title: 'RH SOLUTION REPORTE SEMANAL'
-                }],
-                paging: true
-            });
-            var chartdata = {
-                labels: nombre,
-                datasets: [{
-                    label: nombre,
-                    backgroundColor: color,
-                    borderColor: color,
-                    borderWidth: 2,
-                    hoverBackgroundColor: color,
-                    hoverBorderColor: borderColor,
-                    data: horas
-                }]
-            };
-            var mostrar = $("#myChart");
-            grafico = new Chart(mostrar, {
-                type: 'bar',
-                data: chartdata,
-                theme: "light2",
-                options: {
-                    responsive: true,
-                    scales: {
-                        xAxes: [{
-                            stacked: true,
-                            gridLines: {
-                                display: false
-                            }
-                        }],
-                        yAxes: [{
-                            stacked: true
-                        }]
-                    }
-                }
-            });
+                });
+            } else {
+                $.notify({
+                    message: "No se encontraron datos.",
+                    icon: 'admin/images/warning.svg'
+                });
+            }
         },
         error: function (data) {}
     })
--- resources/views/tareas/reporteSemanal.blade.php
+++ resources/views/tareas/reporteSemanal.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.vertical')
 
 @section('css')
-<link rel="shortcut icon" href="https://rhsolution.com.pe/wp-content/uploads/2019/06/small-logo-rh-solution-64x64.png"
+<link rel="shortcut icon"
+    href="https://rhsolution.com.pe/wp-content/uploads/2019/06/small-logo-rh-solution-64x64.png"
     sizes="32x32">
-<link href="{{asset('admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('admin/assets/css/icons.min.css')}}" rel="stylesheet"
+    type="text/css" />
 <link href="{{
     URL::asset('admin/assets/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.css')
     }}" rel="stylesheet" />
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <link href="{{
     URL::asset('admin/assets/libs/bootstrap-datetimepicker-master/bootstrap-datetimepicker.css')
     }}" rel="stylesheet" type="text/css" />
 <link href="{{
     URL::asset('admin/assets/libs/bootstrap-datetimepicker-master/bootstrap-datetimepicker.min.css')
     }}" rel="stylesheet" type="text/css" />
-<link href="{{ URL::asset('admin/assets/libs/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ URL::asset('admin/assets/libs/chart/Chart.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ URL::asset('admin/assets/css/notify.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ URL::asset('admin/assets/css/prettify.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('admin/assets/libs/datatables/datatables.min.css') }}"
+    rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('admin/assets/libs/chart/Chart.min.css') }}"
+    rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('admin/assets/css/notify.css') }}" rel="stylesheet"
+    type="text/css" />
+<link href="{{ URL::asset('admin/assets/css/prettify.css') }}" rel="stylesheet"
+    type="text/css" />
 <link href="{{
     URL::asset('admin/assets/libs/bootstrap-touchspin/bootstrap-touchspin.min.css')
     }}" rel="stylesheet" />
@@ -47,6 +54,8 @@
     .table-condensed {
         width: 300px !important;
         height: 300px !important;
+        min-width: 50%!important;
+        min-height: 50%!important;
         font-size: small !important;
     }
 </style>
@@ -60,7 +69,8 @@
                         <div class="input-group col-md-12">
                             <input type="text" id="fecha" class="form-control">
                             <div class="input-group-prepend">
-                                <div class="input-group-text form-control"><i class="uil uil-calender"></i></div>
+                                <div class="input-group-text form-control"><i
+                                        class="uil uil-calender"></i></div>
                             </div>
                         </div>
                     </div>
@@ -71,7 +81,8 @@
                         <!-- Portlet card -->
                         <div class="card">
                             <div class="card-body">
-                                <canvas id="myChartD" height="35vh" width="85vw"></canvas>
+                                <canvas id="myChartD" height="35vh"
+                                    width="85vw"></canvas>
                                 <canvas id="myChart" height="35vh" width="85vw"></canvas>
                             </div> <!-- end card-body -->
                         </div> <!-- end card-->
@@ -88,41 +99,46 @@
                                     America-Bogota
                                 </p>
                                 <br>
-                                <table id="Reporte" class="table nowrap" style="font-size: 13px!important;width:
-                                    100%;">
-                                    <thead style="background: #fafafa;" id="dias">
-                                        <tr>
-                                            <th><img src="{{
-                                                    URL::asset('admin/assets/images/users/empleado.png')
-                                                    }}" class="mr-2" alt="" />Miembro</th>
-                                            <th>LUN.</th>
-                                            <th>MAR.</th>
-                                            <th>MIÉ.</th>
-                                            <th>JUE.</th>
-                                            <th>VIE.</th>
-                                            <th>SÁB.</th>
-                                            <th>TOTAL</th>
-                                            <th>ACTIV.</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="empleado">
-                                        @foreach ($empleado as $empleados)
-                                        <tr>
-                                            <td>{{$empleados->perso_nombre}}
-                                                {{$empleados->perso_apPaterno}}
-                                                {{$empleados->perso_apMaterno}}</td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                <div class="table-responsive-xl">
+                                    <table id="Reporte" class="table nowrap"
+                                        style="font-size: 13px!important;width:
+                                        100%;">
+                                        <thead style="background: #fafafa;"
+                                            id="dias">
+                                            <tr>
+                                                <th><img src="{{
+                                                        URL::asset('admin/assets/images/users/empleado.png')
+                                                        }}" class="mr-2" alt=""
+                                                        />Miembro</th>
+                                                <th>LUN.</th>
+                                                <th>MAR.</th>
+                                                <th>MIÉ.</th>
+                                                <th>JUE.</th>
+                                                <th>VIE.</th>
+                                                <th>SÁB.</th>
+                                                <th>TOTAL</th>
+                                                <th>ACTIV.</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="empleado">
+                                            @foreach ($empleado as $empleados)
+                                            <tr>
+                                                <td>{{$empleados->perso_nombre}}
+                                                    {{$empleados->perso_apPaterno}}
+                                                    {{$empleados->perso_apMaterno}}</td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div> <!-- end card body-->
                         </div>
                     </div>
@@ -144,12 +160,18 @@
 {{-- <script src="{{asset('admin/assets/js/app.min.js')}}"></script> --}}
 <!-- datatable js -->
 <script src="{{ URL::asset('admin/assets/libs/chart/Chart.min.js') }}"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<script src="{{ URL::asset('admin/assets/js/notify.js') }}"></script>
+<link rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<script src="{{
+    URL::asset('admin/assets/libs/bootstrap-notify-master/bootstrap-notify.js')
+    }}"></script>
 <script src="{{ URL::asset('admin/assets/js/prettify.js') }}"></script>
-<script src="{{asset('admin/assets/libs/bootstrap-datetimepicker-master/bootstrap-datetimepicker.min.js')}}"></script>
-<script src="{{asset('admin/assets/libs/bootstrap-datetimepicker-master/bootstrap-datetimepicker.es.js')}}"></script>
-<script src="{{asset('admin/assets/libs/bootstrap-datetimepicker-master/moment.js')}}"></script>
+<script
+    src="{{asset('admin/assets/libs/bootstrap-datetimepicker-master/bootstrap-datetimepicker.min.js')}}"></script>
+<script
+    src="{{asset('admin/assets/libs/bootstrap-datetimepicker-master/bootstrap-datetimepicker.es.js')}}"></script>
+<script
+    src="{{asset('admin/assets/libs/bootstrap-datetimepicker-master/moment.js')}}"></script>
 <script src="{{asset('admin/assets/libs/combodate-1.0.7/es.js')}}"></script>
 <script src="{{ URL::asset('admin/assets/js/pages/datatables.init.js') }}"></script>
 <script src="{{ URL::asset('admin/assets/libs/datatables/datatables.min.js')
