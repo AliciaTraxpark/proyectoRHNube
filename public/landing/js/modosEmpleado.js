533bc1baf5fc19ee097009a8d73128bb54f09ff4
GabrielaRosmeri <gaby020313@gmail.com>
2020-08-28 11:23:55 -0500

    todo funciona ok

--- public/landing/js/modosEmpleado.js
+++ public/landing/js/modosEmpleado.js
@@ -17,11 +17,19 @@ function actividad_empleado() {
                 var container = $('#regtablaBodyTarea');
                 var td = '';
                 for (var $i = 0; $i < data.length; $i++) {
-                    td += `<tr><td>${data[$i].Activi_Nombre}</td>`;
+                    td += `<tr onclick="return RegeditarActE(${data[$i].Activi_id})">
+                    <input type="hidden" id="idActReg${data[$i].Activi_id}" value="${data[$i].Activi_Nombre}">
+                    <td class="editable" id="tdActReg${data[$i].Activi_id}">${data[$i].Activi_Nombre}</td>`;
                     if (data[$i].estado == 1) {
-                        td += `<td>Activo</td><td></td></tr>`;
+                        td += `<td><div class="custom-control custom-switch">
+                        <input type="checkbox" checked="" class="custom-control-input" id="customSwitchActReg${data[$i].Activi_id}">
+                        <label class="custom-control-label" for="customSwitchActReg${data[$i].Activi_id}"></label>
+                      </div></td><td></td></tr>`;
                     } else {
-                        td += `<td>Inactivo</td><td></td></tr>`;
+                        td += `<td><div class="custom-control custom-switch">
+                        <input type="checkbox" class="custom-control-input" id="customSwitchActReg${data[$i].Activi_id}">
+                        <label class="custom-control-label" for="customSwitchActReg${data[$i].Activi_id}"></label>
+                      </div></td><td></td></tr>`;
                     }
                 }
                 container.append(td);
@@ -177,6 +185,7 @@ $('#customSwitch6').on('change.bootstrapSwitch', function (event) {
     }
 });
 // **************************************
+// MODAL EDITAR
 function registrarActividadTarea() {
     var idE = $('#v_id').val();
     var nombre = $('#nombreTarea').val();
@@ -218,7 +227,7 @@ function registrarActividadTarea() {
         }
     });
 }
-
+// MODAL REGISTRAR
 function registrarNuevaActividadTarea() {
     var idE = $('#idEmpleado').val();
     var nombre = $('#regnombreTarea').val();
@@ -266,6 +275,83 @@ function limpiarModo() {
     $('#regnombreTarea').val("");
 }
 //  *******************************
+// MODAL REGISTRAR
+function editarActividadReg(id, actividad) {
+    $.ajax({
+        type: "GET",
+        url: "/editarActvE",
+        data: {
+            idA: id,
+            actividad: actividad
+        },
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        success: function (data) {
+            actividad_empleado();
+            $.notifyClose();
+            $.notify({
+                message: "\nActividad Modificada.",
+                icon: 'admin/images/checked.svg'
+            }, {
+                element: $('#form-registrar'),
+                position: 'fixed',
+                icon_type: 'image',
+                newest_on_top: true,
+                delay: 5000,
+                template: '<div data-notify="container" class="col-xs-12 col-sm-3 text-center alert" style="background-color: #dff0d8;" role="alert">' +
+                    '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                    '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span style="color:#3c763d;" data-notify="message">{2}</span>' +
+                    '</div>',
+                spacing: 35
+            });
+        },
+        error: function () {
+
+        }
+    });
+}
+// MODAL REGISTRAR
+function editarEstadoActividadReg(id, estado) {
+    $.ajax({
+        type: "GET",
+        url: "/editarEstadoA",
+        data: {
+            idA: id,
+            estado: estado
+        },
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        success: function (data) {
+            actividad_empleado();
+            $.notifyClose();
+            $.notify({
+                message: "\nEstado Modificado.",
+                icon: 'admin/images/checked.svg'
+            }, {
+                element: $('#form-registrar'),
+                position: 'fixed',
+                icon_type: 'image',
+                newest_on_top: true,
+                delay: 5000,
+                template: '<div data-notify="container" class="col-xs-12 col-sm-3 text-center alert" style="background-color: #dff0d8;" role="alert">' +
+                    '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                    '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span style="color:#3c763d;" data-notify="message">{2}</span>' +
+                    '</div>',
+                spacing: 35
+            });
+        },
+        error: function () {
+
+        }
+    });
+}
+// MODAL EDITAR
 function editarActividad(id, actividad) {
     $.ajax({
         type: "GET",
@@ -303,7 +389,7 @@ function editarActividad(id, actividad) {
         }
     });
 }
-
+// MODAL EDITAR
 function editarEstadoActividad(id, estado) {
     $.ajax({
         type: "GET",
@@ -341,7 +427,77 @@ function editarEstadoActividad(id, estado) {
         }
     });
 }
+// MODAL REGISTRAR
+function RegeditarActE(idA) {
+    var OriginalContent = $('#idActReg' + idA).val();
+    $("#tdActReg" + idA).on('click', function () {
+        console.log(OriginalContent);
+        $(this).addClass("editable");
+        $(this).html("<input type='text' style='border-radius: 5px;border: 1px solid #39c;' value='" + OriginalContent + "'/>");
+        $(this).children().first().focus();
+        $(this).children().first().keypress(function (e) {
+            if (e.which == 13) {
+                var newContent = $(this).val();
+                alertify.confirm("¿Desea modificar nombre de la actividad?", function (e) {
+                    if (e) {
+                        editarActividadReg(idA, newContent);
+                        $(this).parent().text(newContent);
+                        $(this).parent().removeClass("editable")
+                    } else {
+                        $(this).parent().text(OriginalContent);
+                        $(this).parent().removeClass("editable");
+                    }
+                }).setting({
+                    'title': 'Modificar Actividad',
+                    'labels': {
+                        ok: 'Aceptar',
+                        cancel: 'Cancelar'
+                    },
+                    'modal': true,
+                    'startMaximized': false,
+                    'reverseButtons': true,
+                    'resizable': false,
+                    'transition': 'zoom'
+                });
+            }
+        });
 
+        $(this).children().first().blur(function () {
+            $(this).parent().text(OriginalContent);
+            $(this).parent().removeClass("editable");
+        });
+
+    });
+
+    $('#customSwitchActReg' + idA).on('change.bootstrapSwitch', function (event) {
+        if (event.target.checked == true) {
+            var valor = 1;
+        } else {
+            var valor = 0;
+        }
+        console.log(valor);
+        alertify.confirm("¿Desea modificar el estado de la  actividad?", function (e) {
+            if (e) {
+                editarEstadoActividadReg(idA, valor);
+            } else {
+                actividad_empleado();
+            }
+        }).setting({
+            'title': 'Modificar Actividad',
+            'labels': {
+                ok: 'Aceptar',
+                cancel: 'Cancelar'
+            },
+            'modal': true,
+            'startMaximized': false,
+            'reverseButtons': true,
+            'resizable': false,
+            'transition': 'zoom'
+        });
+    });
+}
+
+// MODAL EDITAR
 function editarActE(idA) {
     var OriginalContent = $('#idAct' + idA).val();
     $("#tdAct" + idA).on('click', function () {
