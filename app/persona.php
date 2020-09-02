f939f76660806b7cd9b4c8a508f641d48fa8ea53
alicia131 <aliciarodriguez131@gmail.com>
2020-09-02 09:36:03 -0500

    celu

--- app/persona.php
+++ app/persona.php
@@ -9,5 +9,5 @@ class persona extends Model
     //
     protected $table = 'persona';
     protected $primaryKey = 'perso_id';
-    protected $fillable = ['perso_id','perso_nombre','perso_apPaterno','perso_apMaterno','perso_direccion','perso_fechaNacimiento','perso_sexo'];
+    protected $fillable = ['perso_id','perso_nombre','perso_apPaterno','perso_apMaterno','perso_direccion','perso_fechaNacimiento','perso_sexo','perso_celular'];
 }
