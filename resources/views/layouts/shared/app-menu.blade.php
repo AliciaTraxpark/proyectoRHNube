8bb9ac2d8f60336f684c87ac0d4422daac9eee3e
gabriela sanchez maza <gaby020313@gmail.com>
2020-09-11 07:04:07 -0500

    cambios hechos

--- resources/views/layouts/shared/app-menu.blade.php
+++ resources/views/layouts/shared/app-menu.blade.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\DB;
         padding: 9px 30px;
         font-size: 14px;
     }
-
 </style>
 @php
 $usuario=DB::table('users')
@@ -41,143 +40,156 @@ $usuario_organizacion=DB::table('usuario_organizacion')
                 <span> Calendarios </span>
             </a>
 
-        </li>
-        <li>
+    </li>
+    <li>
 
-            @if ($usuario[0]->user_estado==0)
-            <a href="/empleado">
-                <i data-feather="list"></i>
-                <span>Gestión de empleado</span>
-            </a>
-
-            @else
-            <a href="/empleados">
-                <i data-feather="list"></i>
-                <span>Gestión de empleado</span>
-            </a>
-            @endif
-
-        </li>
-        <!-- <li>
-        <a href="/proyecto">
-            <i data-feather="briefcase"></i>
-            <span>Asignar tarea</span>
+        @if ($usuario[0]->user_estado==0)
+        <a href="/empleado">
+            <i data-feather="list"></i>
+            <span>Gestión de empleado</span>
         </a>
-    </li> -->
-        <li>
 
-            <a href="javascript: void(0);">
-                <i data-feather="clipboard"></i>
-                <span>Horarios</span>
-                <span class="menu-arrow"></span>
-            </a>
-
-            <ul class="nav-second-level" aria-expanded="false">
-                <li>
-
-                    @if ($usuario[0]->user_estado==0)
-                    <a href="/horario">
-
-                        <span>Asignar horario</span>
-                    </a>
-
-                    @else
-                    <a href="/horarios">
-
-                        <span>Asignar horario</span>
-                    </a>
-                    <a href="/dias/laborales">
-
-                        <span>Asignar dias no laborales</span>
-                    </a>
-                    @endif
-
-                </li>
-            </ul>
-        </li>
-
-        <li>
-
-            <a href="javascript: void(0);">
-                <i data-feather="activity"></i>
-                <span>Modulo: Trabajo Remoto</span>
-                <span class="menu-arrow"></span>
-            </a>
-
-            <ul class="nav-second-level" aria-expanded="false">
-                <li>
-                    <a href="/tareas">Detalle diario</a>
-                </li>
-            </ul>
-        </li>
-
-        <li>
-            <a href="javascript: void(0);">
-                <img src="{{asset('admin/images/growth (2).svg')}}" height="25"
-                    class="mr-1">
-                <span>Horas trabajadas</span>
-                <span class="menu-arrow"></span>
-            </a>
-            <ul class="nav-second-level" aria-expanded="false">
-                <li>
-                    <a href="/reporteSemanal">Reporte Semanal</a>
-                    <a href="/reporteMensual">Reporte Mensual</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-    @endif
-    @if ($usuario_organizacion[0]->rol_id==3)
-
-    <ul class="metismenu" id="menu-bar">
-        @php
-        $invitadod=DB::table('invitado')
-        ->where('user_Invitado','=',Auth::user()->id)
-        ->where('organi_id','=',session('sesionidorg'))
-        ->get()->first();
-        @endphp
-        @if ($invitadod->dashboard==1)
-        <li>
-            <a href="/dashboard" id="menuD">
-                <i data-feather="home"></i>
-                <span class="badge badge-success float-right">1</span>
-                <span> Dashboard </span>
-            </a>
-        </li>
-
-
+        @else
+        <a href="/empleados">
+            <i data-feather="list"></i>
+            <span>Gestión de empleado</span>
+        </a>
         @endif
 
-
-
-
-        <!-- <li>
+    </li>
+    <!-- <li>
         <a href="/proyecto">
             <i data-feather="briefcase"></i>
             <span>Asignar tarea</span>
         </a>
     </li> -->
-        <li>
+    <li>
 
+        <a href="javascript: void(0);">
+            <i data-feather="clipboard"></i>
+            <span>Horarios</span>
+            <span class="menu-arrow"></span>
+        </a>
+
+        <ul class="nav-second-level" aria-expanded="false">
+            <li>
+
+                @if ($usuario[0]->user_estado==0)
+                <a href="/horario">
+
+                    <span>Asignar horario</span>
+                </a>
+
+                @else
+                <a href="/horarios">
+
+                    <span>Asignar horario</span>
+                </a>
+                <a href="/dias/laborales">
+
+                    <span>Asignar dias no laborales</span>
+                </a>
+                @endif
+
+            </li>
+        </ul>
+    </li>
+
+    <li>
+
+        <a href="javascript: void(0);">
+            <i data-feather="activity"></i>
+            <span>Modo: Control Remoto</span>
+            <span class="menu-arrow"></span>
+        </a>
+
+        <ul class="nav-second-level" aria-expanded="false">
+            <li>
+                <a href="/tareas"><img src="{{asset('admin/images/ver.svg')}}" height="25" class="mr-1">Detalle
+                    diario</a>
+            </li>
+            <li>
+                <a href="javascript: void(0);">
+                    <img src="{{asset('admin/images/growth (2).svg')}}" height="25" class="mr-1">
+                    <span>Horas trabajadas</span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <ul class="nav-second-level" aria-expanded="false">
+                    <li>
+                        <a href="/reporteSemanal">Reporte Semanal</a>
+                        <a href="/reporteMensual">Reporte Mensual</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </li>
+    {{-- 
+        <li>
             <a href="javascript: void(0);">
-                <i data-feather="activity"></i>
-                <span>Modulo TASK</span>
-                <span class="menu-arrow"></span>
-            </a>
-
-            <ul class="nav-second-level" aria-expanded="false">
-                <li>
-                    <a href="/tareas">Detalle diario</a>
-                </li>
-            </ul>
-        </li>
-
-        <li>
-            <a href="/reporteSemanal">
                 <img src="{{asset('admin/images/growth (2).svg')}}" height="25"
-                    class="mr-1">
-                <span>Informe semanal</span>
-            </a>
+    class="mr-1">
+    <span>Horas trabajadas</span>
+    <span class="menu-arrow"></span>
+    </a>
+    <ul class="nav-second-level" aria-expanded="false">
+        <li>
+            <a href="/reporteSemanal">Reporte Semanal</a>
+            <a href="/reporteMensual">Reporte Mensual</a>
         </li>
     </ul>
+    </li> --}}
+</ul>
+@endif
+@if ($usuario_organizacion[0]->rol_id==3)
+
+<ul class="metismenu" id="menu-bar">
+    @php
+    $invitadod=DB::table('invitado')
+    ->where('user_Invitado','=',Auth::user()->id)
+    ->where('organi_id','=',session('sesionidorg'))
+    ->get()->first();
+    @endphp
+    @if ($invitadod->dashboard==1)
+    <li>
+        <a href="/dashboard" id="menuD">
+            <i data-feather="home"></i>
+            <span class="badge badge-success float-right">1</span>
+            <span> Dashboard </span>
+        </a>
+    </li>
+
+
     @endif
+
+
+
+
+    <!-- <li>
+        <a href="/proyecto">
+            <i data-feather="briefcase"></i>
+            <span>Asignar tarea</span>
+        </a>
+    </li> -->
+    <li>
+
+        <a href="javascript: void(0);">
+            <i data-feather="activity"></i>
+            <span>Modulo TASK</span>
+            <span class="menu-arrow"></span>
+        </a>
+
+        <ul class="nav-second-level" aria-expanded="false">
+            <li>
+                <a href="/tareas">Detalle diario</a>
+            </li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="/reporteSemanal">
+            <img src="{{asset('admin/images/growth (2).svg')}}" height="25" class="mr-1">
+            <span>Informe semanal</span>
+        </a>
+    </li>
+</ul>
+@endif
