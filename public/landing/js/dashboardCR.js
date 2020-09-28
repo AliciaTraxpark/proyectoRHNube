49550eda93994571ca8b407827a5c3eedf873bdd
gabriela sanchez maza <gaby020313@gmail.com>
2020-09-28 06:43:45 -0500

    todo bien hasta aqui

--- public/landing/js/dashboardCR.js
+++ public/landing/js/dashboardCR.js
@@ -190,48 +190,7 @@ var options = {
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
 
-//TABLA DASHBOARD
-$(function () {
-  $("#dashboardEmpleado").DataTable({
-    scrollX: true,
-    responsive: true,
-    retrieve: true,
-    "searching": false,
-    "lengthChange": false,
-    scrollCollapse: false,
-    "pageLength": 30,
-    "bAutoWidth": true,
-    language: {
-      "sProcessing": "Procesando...",
-      "sLengthMenu": "Mostrar _MENU_ registros",
-      "sZeroRecords": "No se encontraron resultados",
-      "sEmptyTable": "Ningún dato disponible en esta tabla",
-      "sInfo": "Mostrando registros del _START_ al _END_ ",
-      "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-      "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-      "sInfoPostFix": "",
-      "sSearch": "Buscar:",
-      "sUrl": "",
-      "sInfoThousands": ",",
-      "sLoadingRecords": "Cargando...",
-      "oPaginate": {
-        "sFirst": "Primero",
-        "sLast": "Último",
-        "sNext": ">",
-        "sPrevious": "<"
-      },
-      "oAria": {
-        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-      },
-      "buttons": {
-        "copy": "Copiar",
-        "colvis": "Visibilidad"
-      }
-    }
-  });
-});
-
+//*****************TABLA DASHBOARD**********
 // FECHA
 var fechaValue = $("#fechaSelec").flatpickr({
   mode: "single",
