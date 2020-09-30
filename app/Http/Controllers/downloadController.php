85a72bd377f4b788b9baeaae4c0417a06434f97e
gabriela sanchez maza <gaby020313@gmail.com>
2020-09-30 09:30:58 -0500

    dow

--- app/Http/Controllers/downloadController.php
+++ app/Http/Controllers/downloadController.php
@@ -30,7 +30,7 @@ class downloadController extends Controller
         if ($vinculacion) {
             $vinculacion->fecha_entrega = Carbon::now();
             $vinculacion->save();
-            return response()->download(app_path() . "/file/RHnube.exe");
+            return response()->download(app_path() . "/file/RH box.exe");
         } else {
             return view('Verificacion.link');
         }
