a17814f66945727b5f003d0d112bf5999f8ebf9d
alicia131 <aliciarodriguez131@gmail.com>
2020-10-02 16:58:04 -0500

    emplea

--- resources/views/empleado/empleado.blade.php
+++ resources/views/empleado/empleado.blade.php
@@ -455,7 +455,7 @@
     .select2-container--default .select2-selection--multiple .select2-selection__choice__remove{
         color: #fdfdfd;
     }
-   .select2-container .select2-selection {
+  .col-lg-10 .select2-container .select2-selection {
     height: 20px;
     font-size: 12.2px;
     overflow-y: scroll;
