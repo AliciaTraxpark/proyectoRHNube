96b4275940bc2fe3184f257892d4c5d0367005fc
alicia131 <aliciarodriguez131@gmail.com>
2020-10-05 18:14:54 -0500

    api

--- app/horario.php
+++ app/horario.php
@@ -9,6 +9,7 @@ class horario extends Model
     //
     protected $table = 'horario';
     protected $primaryKey = 'horario_id';
-    protected $fillable = ['horario_id','horario_sobretiempo','horario_tipo','horario_descripcion','horario_tolerancia','horaI','horaF','organi_id'];
+    protected $fillable = ['horario_id','horario_sobretiempo','horario_tipo','horario_descripcion',
+    'horario_tolerancia','horaI','horaF','organi_id','horario_toleranciaF'];
     public $timestamps = false;
 }
