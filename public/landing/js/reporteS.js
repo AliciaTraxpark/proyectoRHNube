ab55e5f1ab7f2cf19d2367120a3bcfd743a6f4bf
gabriela sanchez maza <gaby020313@gmail.com>
2020-10-06 17:59:58 -0500

    todo bien hasta aqui

--- public/landing/js/reporteS.js
+++ public/landing/js/reporteS.js
@@ -20,7 +20,82 @@ var notify = $.notifyDefaults({
         '<span style="color:#8a6d3b" data-notify="message">{2}</span>' +
         '</div>'
 });
-
+$(function () {
+    $("#Reporte").DataTable({
+        "searching": false,
+        "scrollX": true,
+        retrieve: true,
+        "ordering": false,
+        "pageLength": 15,
+        "autoWidth": false,
+        "lengthChange": false,
+        language: {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ ",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": ">",
+                "sPrevious": "<"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+        }
+    });
+    $("#actividadD").DataTable({
+        "searching": false,
+        "scrollX": true,
+        retrieve: true,
+        "ordering": false,
+        "pageLength": 15,
+        "autoWidth": false,
+        "lengthChange": false,
+        language: {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ ",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": ">",
+                "sPrevious": "<"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+        }
+    });
+});
 function acumular60(suma, acumulado) {
     if (suma > 60) {
         suma = suma - 60;
