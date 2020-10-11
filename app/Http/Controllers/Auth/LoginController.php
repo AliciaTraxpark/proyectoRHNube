596596580627d975095168427231988b6493a316
alicia131 <aliciarodriguez131@gmail.com>
2020-10-10 22:01:33 -0500

    ver

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use App\usuario_organizacion;
 use App\User;
 use App\invitado;
+use Illuminate\Support\Facades\Crypt;
 class LoginController extends Controller
 {
     /*
@@ -64,37 +65,46 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             Auth::logoutOtherDevices(request()->input('password'));
             $usuario_organizacion=usuario_organizacion::where('user_id','=', Auth::user()->id)->get()->first();
-            if($usuario_organizacion->rol_id==4){
-                return redirect('/superadmin');
-            }
-            else{
-                $comusuario_organizacion=usuario_organizacion::where('user_id','=', Auth::user()->id)->count();
-                if($comusuario_organizacion>1) {
 
-
-                    return redirect(route('elegirorganizacion'));
-                       /* ->with('elegirEmpresa',' Elige tu empresa')  */;
-                }else{
-
-                    $vars=$usuario_organizacion->organi_id;
-                    session(['sesionidorg' => $vars]);
-                    ///
-                    $invitado=invitado::where('user_Invitado','=', Auth::user()->id)
-                    ->where('organi_id','=', session('sesionidorg'))
-                    ->where('estado_condic','=', 0)->get()->first();
-                    /////
-                    if($invitado){
-                        Auth::logout();
-                        session()->forget('sesionidorg');
-                        /* return view ('welcome'); */
-                        return redirect()->route('login')
-                        ->with('error', 'Usuario no activado.');
-                    }else{
-                        return redirect(route('dashboard'));
-                    }
-
+            if($usuario_organizacion!=null){
+                if($usuario_organizacion->rol_id==4){
+                    return redirect('/superadmin');
                 }
+                else{
+                    $comusuario_organizacion=usuario_organizacion::where('user_id','=', Auth::user()->id)->count();
+                    if($comusuario_organizacion>1) {
+
+
+                        return redirect(route('elegirorganizacion'));
+                           /* ->with('elegirEmpresa',' Elige tu empresa')  */;
+                    }else{
+
+                        $vars=$usuario_organizacion->organi_id;
+                        session(['sesionidorg' => $vars]);
+                        ///
+                        $invitado=invitado::where('user_Invitado','=', Auth::user()->id)
+                        ->where('organi_id','=', session('sesionidorg'))
+                        ->where('estado_condic','=', 0)->get()->first();
+                        /////
+                        if($invitado){
+                            Auth::logout();
+                            session()->forget('sesionidorg');
+                            /* return view ('welcome'); */
+                            return redirect()->route('login')
+                            ->with('error', 'Usuario no activado.');
+                        }else{
+                            return redirect(route('dashboard'));
+                        }
+
+                    }
+                }
+            } else{
+                $id = Auth::user()->id;
+                $user1 = Crypt::encrypt($id);
+                return redirect('/registro/organizacion/'+$user1);
             }
+
+
 
 
         } else {
