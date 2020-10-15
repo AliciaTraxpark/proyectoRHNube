0ad23b58bb37db3ea1587a2b4ef0314be754169e
gabriela sanchez maza <gaby020313@gmail.com>
2020-10-15 07:56:04 -0500

    middleware

--- app/Http/Controllers/ActividadesController.php
+++ app/Http/Controllers/ActividadesController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class ActividadesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
     public function actividadesEmpleado(Request $request)
     {
         $respuesta = [];
