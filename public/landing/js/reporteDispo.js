57a37ed64bd9790d5aad7670115ea7955bc90e24
alicia131 <aliciarodriguez131@gmail.com>
2020-10-19 17:52:33 -0500

    casi listo

--- public/landing/js/reporteDispo.js
+++ public/landing/js/reporteDispo.js
@@ -1,3 +1,5 @@
+
+
 // FECHA
 var fechaValue = $("#fechaSelec").flatpickr({
     mode: "single",
@@ -16,17 +18,268 @@ var fechaValue = $("#fechaSelec").flatpickr({
 
   });
 function cargartabla (fecha) {
- console.log(fecha);
-    var table =
+    $('#tableZoom').empty();
+    $('#tableZoom').html(' <table id="tablaReport" class="table  nowrap" style="font-size: 12.8px;">'+
+    '<thead id="datosHtm" style=" background: #edf0f1;color: #6c757d;"></thead>'+
+    '<tbody id="tbodyD" style="">'+
+    '</tbody></table>');
+  /*   $('#datosHtm').empty();
+    $('#tbodyD').empty(); */
+   /*  if ($.fn.DataTable.isDataTable("#tablaReport")) {
+        $('#tablaReport').DataTable().destroy();
+    } */
 
 
+
+    $.ajax({
+        type: "POST",
+        url: "/reporteTablaMarca",
+        data: {
+           fecha
+        },
+        async:false,
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        statusCode: {
+            /*401: function () {
+                location.reload();
+            },*/
+            419: function () {
+                location.reload();
+            }
+        },
+        success: function (data) {
+
+            thead='<tr>'+
+            '<th>CC</th>'+
+            '<th>DNI</th>'+
+            '<th>Nombre</th>'+
+            '<th>Cargo</th>'+
+            '<th id="hEntrada">Hora de entrada</th>'+
+            '<th id="hSalida">Hora de salida</th>'+
+            '<th id="tSitio">Tiempo en sitio</th>'+
+        '</tr>'
+
+        $('#datosHtm').html(thead);
+        if(data.length> 0  ){
+            var cadena=[];
+             $.each( JSON.parse(data), function( index, value ){
+                nDatos1=value.entrada;
+                nEntradas=nDatos1.split(',');
+                cadena.push(nEntradas.length);
+            })
+            nclonar=Math.max(...cadena);
+            $('<input type="hidden" id="nfila"></input>').insertAfter($('#tablaReport'));
+            $('#nfila').val(nclonar);
+             var e = $('#tSitio');
+
+            for (var i = 0; i < nclonar-1; i++) {
+                $("<th>Hora de entrada</th><th>Hora de salida</th><th>Tiempo en sitio</th>").insertAfter(e);
+             }
+        }
+
+
+
+        },
+        error: function () {}
+    });
+    $.ajax({
+        type: "POST",
+        url: "/reporteTablaMarca",
+        data: {
+           fecha
+        },
+        async:false,
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        statusCode: {
+            /*401: function () {
+                location.reload();
+            },*/
+            419: function () {
+                location.reload();
+            }
+        },
+        success: function (data) {
+            var tbodyTabla=[];
+
+            dataA=JSON.parse(data)
+
+            for (var i = 0; i < dataA.length; i++) {
+        tbody='<tr>'+
+        '<td>' + (i + 1) +'</td>'+
+        '<td>'+dataA[i].emple_nDoc+'</td>'+
+        '<td>'+dataA[i].perso_nombre+dataA[i].perso_apPaterno+dataA[i].perso_apMaterno+'</td>';
+
+        if(dataA[i].cargo_descripcion!=null){
+        tbody+=
+            '<td>'+ dataA[i].cargo_descripcion+
+        '</td>';
+        }
+        else{
+            tbody+=
+            '<td> ---</td>';
+        }
+
+        var nfi=$('#nfila').val();
+         let cuerpoA='';
+         for (var b = 0; b < nfi; b++) {
+            //entrada
+            if(dataA[i].entrada!=0 || dataA[i].entrada!=null ){
+                vectorEntrada=dataA[i].entrada;
+                nEntradas=vectorEntrada.split(',');
+
+
+                cuerpoTDB='';
+                cuerpoVacioTd='';
+                if(nEntradas.length<2){
+                    if(b==0){
+                       if( nEntradas[0]!=0){
+                        cuerpo = '<td><img style="margin-bottom: 3px;" src="landing/images/entradaD.svg" class="mr-2" height="12"/>'+moment(nEntradas[0]).format("HH:mm:ss") +'</td>';
+                       } else{
+                        cuerpo= '<td><span class="badge badge-soft-warning"><img style="margin-bottom: 3px;" src="landing/images/warning.svg" class="mr-2" height="12"/>No tiene entrada</span></td> ';
+                    }
+
+                    } else{
+                        cuerpo = '<td>----</td>';
+                    }
+
+                }else{
+                for(var z = 0; z < nEntradas.length; z++){
+                    if(nEntradas[b]!=0){
+                        cuerpo=  '<td><img style="margin-bottom: 3px;" src="landing/images/entradaD.svg" class="mr-2" height="12"/>'+moment(nEntradas[b]).format("HH:mm:ss")+'</td>';
+                    }
+                    else{
+                        cuerpo= '<td><span class="badge badge-soft-warning"><img style="margin-bottom: 3px;" src="landing/images/warning.svg" class="mr-2" height="12"/>No tiene entrada</span></td> ';
+                    }
+
+                }
+            }
+
+           }
+           else{
+            cuerpo= '<td><span class="badge badge-soft-warning"><img style="margin-bottom: 3px;" src="landing/images/warning.svg" class="mr-2" height="12"/>No tiene entrada</span></td> ';
+
+           }
+           //salida
+           if(dataA[i].final!=0 || dataA[i].final!=null ){
+            vectorSalida=dataA[i].final;
+            nSalidas=vectorSalida.split(',');
+
+             if(nSalidas.length<2){
+                if(b==0){
+                    if( nSalidas[0]!=0){
+                    cuerpo+= '<td><img style="margin-bottom: 3px;" src="landing/images/salidaD.svg" class="mr-2" height="12"/>'+moment(nSalidas[0]).format("HH:mm:ss") +'</td>';}
+                    else{
+                        cuerpo+= '<td><span class="badge badge-soft-secondary"><img style="margin-bottom: 3px;" src="landing/images/wall-clock (1).svg" class="mr-2" height="12"/>No tiene salida</span></td> ';
+                    }
+                } else{
+                    cuerpo+= '<td>--</td>';
+                }
+
+            }else{
+
+                if(nSalidas[b]!=0){
+                    cuerpo+=  '<td><img style="margin-bottom: 3px;" src="landing/images/salidaD.svg" class="mr-2" height="12"/>'+moment(nSalidas[b]).format("HH:mm:ss")+'</td>';
+                }
+                else{
+                    cuerpo+= '<td><span class="badge badge-soft-secondary"><img style="margin-bottom: 3px;" src="landing/images/wall-clock (1).svg" class="mr-2" height="12"/>No tiene salida</span></td> ';
+                }
+
+           }
+           }
+            else{
+           cuerpo+= '<td><span class="badge badge-soft-secondary"><img style="margin-bottom: 3px;" src="landing/images/wall-clock (1).svg" class="mr-2" height="12"/>No tiene salida</span></td> ';
+           }
+
+          //resta
+           if(dataA[i].final!=0 && dataA[i].final!=null && dataA[i].entrada!=0 &&  dataA[i].entrada!=null){
+
+
+
+            if(nSalidas.length<2){
+                if(b==0){
+                    tfinal=moment(nSalidas[0]);
+                    tInicio=moment(nEntradas[0]);
+                    if(tfinal>=tInicio){
+                        tiempo=tfinal-tInicio;
+                    var seconds = moment.duration(tiempo).seconds();
+                    var minutes = moment.duration(tiempo).minutes();
+                    var hours = Math.trunc(moment.duration(tiempo).asHours());
+                    if(hours<10){
+                        hours='0'+hours;
+                    }
+                    if(minutes<10){
+                        minutes='0'+minutes;
+                    }
+
+                    if(seconds<10){
+                        seconds='0'+seconds;
+                    }
+
+                    cuerpo+=  '<td><a class="badge badge-soft-primary mr-2"><img src="landing/images/wall-clock (1).svg" height="12" class="mr-2">'+hours+':'+minutes+':'+seconds+'</a></td>';
+                    }
+                    else{
+                        cuerpo+= '<td><span class="badge badge-soft-secondary"><img style="margin-bottom: 3px;" src="landing/images/wall-clock (1).svg" class="mr-2" height="12"/>---</span></td> ';
+                    }
+                } else{
+                    cuerpo+= '<td>--</td>';
+                }
+
+            }else{
+
+                tfinalV=moment(nSalidas[b]);
+                tInicioV=moment(nEntradas[b]);
+                rrestaConsole=tfinalV[1]-tInicioV[1];
+                console.log('resta'+rrestaConsole);
+                if(tfinalV>=tInicioV && nEntradas[b]!=0 ){
+                    tiempoV=tfinalV-tInicioV;
+                var secondsV = moment.duration(tiempoV).seconds();
+                var minutesV = moment.duration(tiempoV).minutes();
+                var hoursV = Math.trunc(moment.duration(tiempoV).asHours());
+                if(hoursV<10){
+                    hoursV='0'+hoursV;
+                }
+                if(minutesV<10){
+                    minutesV='0'+minutesV;
+                }
+
+                if(secondsV<10){
+                    secondsV='0'+secondsV;
+                }
+                cuerpo+=  '<td><a class="badge badge-soft-primary mr-2"><img src="landing/images/wall-clock (1).svg" height="12" class="mr-2">'+hoursV+':'+minutesV+':'+secondsV+'</a></td>';
+                }  else{
+                    cuerpo+= '<td><span class="badge badge-soft-secondary"><img style="margin-bottom: 3px;" src="landing/images/wall-clock (1).svg" class="mr-2" height="12"/>---</span></td> ';
+                }
+
+           }
+
+           } else{
+            cuerpo+='<td>--</td>';
+           }
+
+            cuerpoA=cuerpo+cuerpoA;
+         }
+
+
+
+          tbody+=cuerpoA;
+         tbody+='</tr>';
+
+    tbodyTabla.push(tbody);
+    }
+    $('#tbodyD').html(tbodyTabla);
+
+    table =
     $("#tablaReport").DataTable({
 
-        "destroy": true,
-        "searching": true,
-        /* "lengthChange": false,
-         "scrollX": true, */
-        "processing": true,
+        "searching": false,
+        "scrollX": true,
+
+        "ordering": false,
+        "autoWidth": true,
 
         language: {
             sProcessing: "Procesando...",
@@ -60,26 +313,49 @@ function cargartabla (fecha) {
             },
         },
 
-        ajax: {
+      /*   ajax: {
 
             type: "post",
-            url: "/reporteTablaMarca",
-            data:{fecha},
+            url: "/reporteTablaMarca", */
+           /*  complete: function (data) {
+                dataD=data['responseJSON'];
+ */
+
+               /*  $('#tablaReport>thead').append(
+               ' <tr>'+
+                    '<th></th>'+
+                    '<th>DNI</th>'+
+                    '<th>Nombre</th>'+
+                    '<th>Cargo</th>'+
+                    '<th>Hora de entrada</th>'+
+                    '<th>Hora de salida</th>'+
+                    '<th>Tiempo en sitio</th>'+
+                '</tr>'
+            ) */
+               /*  $.each(dataD, function (index, value1) {
+                    console.log(value1.emple_id);
+                    $('#nombreEmpleado').find('option[value="' + value1.emple_id + '"]').remove();
+
+
+                $('#nombreEmpleado').select2({});
+                 }) */
+           /*  }, */
+         /*    data:{fecha},
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
 
             "dataSrc": ""
-        },
+        }, */
 
-        "columnDefs": [{
+      /*   "columnDefs": [{
             "searchable": false,
             "orderable": false,
             "targets": 0
         }
         ],
-        "order": [[1, 'asc']],
-        columns: [
+        "order": [[1, 'asc']], */
+       /*  columns: [
             { data: null },
             {
                 data: "emple_nDoc",
@@ -91,29 +367,24 @@ function cargartabla (fecha) {
 
             }},
             { data: "cargo_descripcion" },
+
             {
-                data: "marcaMov_fecha",
+                data: "entrada",
                 "render": function (data, type, row) {
-                    if(row.marcaMov_fecha!=null || row.marcaMov_fecha!=null ){
-                        return  '<img style="margin-bottom: 3px;" src="landing/images/entradaD.svg" class="mr-2" height="12"/>'+ moment(row.marcaMov_fecha).format("HH:mm:ss")+'';
+                    if(row.entrada!=0 ){
+                        return  '<img style="margin-bottom: 3px;" src="landing/images/entradaD.svg" class="mr-2" height="12"/>'+ moment(row.entrada).format("HH:mm:ss")+'';
                    }
                    else{
                        return '<span class="badge badge-soft-warning"><img style="margin-bottom: 3px;" src="landing/images/warning.svg" class="mr-2" height="12"/>No tiene entrada</span> ';
                    }
                 }
             },
-           /*  {
-                data: "users",
-                "render": function (data, type, row) {
-                   return '<label style="font-style:oblique">Empleados de org.</label>'+row.organi_nempleados+'<br>'+
-                   '<label style="font-style:oblique">Empleados regist.</label>'+row.nemple ;
-                }
-            },*/
+
             { data: "final" ,
                 "render": function (data, type, row) {
                     tfinal=moment(row.final);
-                    tInicio=moment(row.marcaMov_fecha);
-                    if(row.marcaMov_fecha!=null || row.marcaMov_fecha!=null ){
+                    tInicio=moment(row.entrada);
+                    if(row.entrada!=0 ){
                         if(tfinal>=tInicio){
 
                             return  '<img style="margin-bottom: 3px;" src="landing/images/salidaD.svg" class="mr-2" height="12"/>'+ moment(row.final).format("HH:mm:ss")+'';
@@ -129,12 +400,10 @@ function cargartabla (fecha) {
             { data: "final" ,
             "render": function (data, type, row) {
                 tfinal=moment(row.final);
-                    tInicio=moment(row.marcaMov_fecha);
+                    tInicio=moment(row.entrada);
                     if(tfinal>=tInicio){
                 tiempo=tfinal-tInicio;
-               /*  return moment(tiempo,"HH:mm:ss");  */
 
-            /*    resta = moment.utc(tiempo*1).format('HH:mm:ss'); */
             var seconds = moment.duration(tiempo).seconds();
             var minutes = moment.duration(tiempo).minutes();
             var hours = Math.trunc(moment.duration(tiempo).asHours());
@@ -155,20 +424,44 @@ function cargartabla (fecha) {
                     return '---';
                 }
             }},
-        ]
+            { data: "cargo_descripcion" },
+            { data: "cargo_descripcion" },
+            { data: "cargo_descripcion" },
+            { data: "cargo_descripcion" },
+            { data: "cargo_descripcion" }, { data: "cargo_descripcion" },
+            {"render": function (data, type, row) {
+               return  {data: "cargo_descripcion"};
+            },}
+        ], */});
+
+        },
+        complete : function(){
+
+
+
+
+        },
+        error: function () {}
     });
 
-    table.on('order.dt search.dt', function () {
+
+
+  /*   table.on('order.dt search.dt', function () {
         table.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
             cell.innerHTML = i + 1;
         });
-    }).draw();
+    }).draw(); */
+
 };
 
 
 function cambiarF(){
+
     f1 = $("#fechaInput").val();
     f2=moment(f1).format("YYYY-MM-DD");
-    console.log(f2);
+    if ($.fn.DataTable.isDataTable("#tablaReport")) {
+
+        /* $('#tablaReport').DataTable().destroy(); */
+    }
     cargartabla(f2);
 }
