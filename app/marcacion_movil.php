3fb50fc1a000f881fd4cf9ac086752e22fdbb64c
alicia131 <aliciarodriguez131@gmail.com>
2020-10-26 10:36:22 -0500

    marca

--- app/marcacion_movil.php
+++ app/marcacion_movil.php
@@ -10,6 +10,7 @@ class marcacion_movil extends Model
     protected $table = 'marcacion_movil';
     protected $primaryKey = 'marcaMov_id';
     protected $fillable = ['marcaMov_id','marcaMov_salida','marcaMov_fecha','marcaMov_emple_id',
-    'controladores_idControladores','dispositivos_idDispositivos','organi_id','horarioEmp_id'];
+    'controladores_idControladores','dispositivos_idDispositivos','organi_id','horarioEmp_id',
+    'marca_latitud','marca_longitud'];
     public $timestamps = false;
 }
