1b69a0f30fec23f85f1bc1395b4a4b0a57403af0
gabriela sanchez maza <gaby020313@gmail.com>
2020-11-03 11:27:58 -0500

    todo bien hasta aqui

--- public/landing/js/dispositivos.js
+++ public/landing/js/dispositivos.js
@@ -715,6 +715,24 @@ function editarNumero(id, numero) {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         },
         success: function (data) {
+            $.notifyClose();
+            $.notify({
+                message: "\nActualización exitosa.",
+                icon: 'admin/images/checked.svg'
+            }, {
+                element: $('#form-ver'),
+                position: 'fixed',
+                icon_type: 'image',
+                newest_on_top: true,
+                delay: 5000,
+                template: '<div data-notify="container" class="col-xs-8 col-sm-2 text-center alert" style="background-color: #dff0d8;" role="alert">' +
+                    '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                    '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span style="color:#3c763d;" data-notify="message">{2}</span>' +
+                    '</div>',
+                spacing: 35
+            });
             dispositivosAndroid();
         },
         error: function () { }
