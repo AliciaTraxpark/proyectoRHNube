0a840f90c528ad9b671b597131a4aeb0477695b3
gabriela sanchez maza <gaby020313@gmail.com>
2020-11-09 19:03:38 -0500

    aqui funciona bien array

--- app/Http/Controllers/ControlController.php
+++ app/Http/Controllers/ControlController.php
@@ -500,6 +500,7 @@ class ControlController extends Controller
     public function showConRuta(Request $request)
     {
 
+        //? FUNCION PARA CAPTURA
         function controlRRJson($array)
         {
             $resultado = array();
@@ -517,8 +518,28 @@ class ControlController extends Controller
             return array_values($resultado);
         }
 
+        // ? FUNCION PARA RUTA
+        function controlRJson($array)
+        {
+            $resultado = array();
+
+            foreach ($array as $ubicacion) {
+                $horaUbicacion = explode(":", $ubicacion->hora);
+                if (!isset($resultado[$horaUbicacion[0]])) {
+                    $resultado[$horaUbicacion[0]] = array("horaUbicacion" => $horaUbicacion[0], "minutos" => array());
+                }
+                if (!isset($resultado[$horaUbicacion[0]]["minutos"][$horaUbicacion[1][0]])) {
+                    $resultado[$horaUbicacion[0]]["minutos"][$horaUbicacion[1][0]] = array();
+                }
+                array_push($resultado[$horaUbicacion[0]]["minutos"][$horaUbicacion[1][0]], $ubicacion);
+            }
+
+            return array_values($resultado);
+        }
+
         $idempleado = $request->get('value');
         $fecha = $request->get('fecha');
+        // ? DATOS DE CAPTURAS
         $control = DB::table('empleado as e')
             ->join('captura as cp', 'cp.idEmpleado', '=', 'e.emple_id')
             ->join('actividad as a', 'a.Activi_id', '=', 'cp.idActividad')
@@ -557,7 +578,88 @@ class ControlController extends Controller
             $c->imagen = $datos;
         }
         $control = controlRRJson($control);
-        return response()->json($control, 200);
+        // ? DATOS DE UBICACIONES
+        $control_ruta = DB::table('empleado as e')
+            ->join('ubicacion as u', 'u.idEmpleado', '=', 'e.emple_id')
+            ->join('actividad as a', 'a.Activi_id', '=', 'u.idActividad')
+            ->leftJoin('horario_dias as hd', 'hd.id', '=', 'u.idHorario_dias')
+            ->select(
+                DB::raw('IF(hd.id is null, DATE(u.hora_ini), DATE(hd.start))'),
+                'a.Activi_Nombre',
+                'u.id as idUbicacion',
+                DB::raw('DATE(u.hora_ini) as fecha'),
+                DB::raw('TIME(u.hora_ini) as hora'),
+                DB::raw('TIME(u.hora_ini) as hora_ini'),
+                DB::raw('TIME(u.hora_fin) as hora_fin')
+            )
+            ->where(DB::raw('IF(hd.id is null, DATE(u.hora_ini), DATE(hd.start))'), '=', $fecha)
+            ->where('e.emple_id', '=', $idempleado)
+            ->where('e.organi_id', '=', session('sesionidorg'))
+            ->orderBy('u.hora_ini', 'asc')
+            ->get();
+
+        foreach ($control_ruta as $cr) {
+            $ubicaciones = DB::table('ubicacion_ruta as ur')
+                ->select('ur.latitud_ini', 'ur.longitud_ini', 'ur.latitud_fin', 'ur.longitud_fin')
+                ->where('ur.idUbicacion', '=', $cr->idUbicacion)
+                ->get();
+            $datos = [];
+            foreach ($ubicaciones as $u) {
+                array_push($datos, $u);
+            }
+            $cr->ubicaciones = $datos;
+        }
+        $control_ruta = controlRJson($control_ruta);
+
+        // dd($control, $control_ruta);
+        $respuesta = array();
+        // ? UNIR LOS DOS ARRAYS
+        // dd(sizeof($control));
+        if (!empty($control) && !empty($control_ruta)) {
+            $menor = array();
+            if ($control[0]["horaCaptura"] < $control_ruta[0]["horaUbicacion"]) {
+                $menor = array("hora" => $control[0]["horaCaptura"], "minuto");
+            } else {
+                $menor = array("hora" => $control[0]["horaUbicacion"], "minuto");
+            }
+            for ($index = sizeof($control) - 1; $index >= 0; $index--) {
+                for ($element = sizeof($control_ruta) - 1; $element >= 0; $element--) {
+                    if ($control[$index]["horaCaptura"] > $control_ruta[$element]["horaUbicacion"]) {
+                        if (empty($respuesta)) {
+                            $respuesta[sizeof($respuesta)] = array("hora" => $control[$index]["horaCaptura"], "minuto" => array());
+                        } else {
+                            $respuestaBusqueda = false;
+                            foreach ($respuesta as $key => $value) {
+                                if ($value["hora"] == $control[$index]["horaCaptura"]) {
+                                    $respuestaBusqueda = true;
+                                }
+                            }
+                            if ($respuestaBusqueda == false) {
+                                $respuesta[sizeof($respuesta)] = array("hora" => $control[$index]["horaCaptura"], "minuto" => array());
+                            }
+                        }
+                    } else {
+                        if (empty($respuesta)) {
+                            $respuesta[sizeof($respuesta)] = array("hora" => $control_ruta[$element]["horaUbicacion"], "minuto" => array());
+                        } else {
+                            $respuestaBusqueda = false;
+                            foreach ($respuesta as $key => $value) {
+                                if ($value["hora"] == $control_ruta[$element]["horaUbicacion"]) {
+                                    $respuestaBusqueda = true;
+                                }
+                            }
+                            if ($respuestaBusqueda == false) {
+                                $respuesta[sizeof($respuesta)] = array("hora" => $control_ruta[$element]["horaUbicacion"], "minuto" => array());
+                            }
+                        }
+                    }
+                }
+            }
+            $respuesta[sizeof($respuesta)] = $menor;
+        }
+        dd($control, $control_ruta);
+        $respuestaMostrar = array_reverse($respuesta);
+        return response()->json($respuestaMostrar, 200);
     }
 
 
