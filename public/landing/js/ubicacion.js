90c5fcb7c90684d4b1b5e6673f57a79096d44f65
gabriela sanchez maza <gaby020313@gmail.com>
2020-11-17 16:50:03 -0500

    todo bien hasta aqui

--- public/landing/js/ubicacion.js
+++ public/landing/js/ubicacion.js
@@ -590,15 +590,6 @@ function ubicacionesMapa(horayJ) {
         }]
 
     }).addTo(map);
-    // L.control.fullscreen({
-    //     position: 'topleft', // change the position of the button can be topleft, topright, bottomright or bottomleft, defaut topleft
-    //     title: 'Show me the fullscreen !', // change the title of the button, default Full Screen
-    //     titleCancel: 'Exit fullscreen mode', // change the title of the button when fullscreen is on, default Exit Full Screen
-    //     content: null, // change the content of the button, can be HTML, default null
-    //     forceSeparateButton: true, // force seperate button to detach from zoom buttons, default false
-    //     forcePseudoFullscreen: true, // force use of pseudo full screen even if full screen API is available, default false
-    //     fullscreenElement: false // Dom element to render in full screen, false by default, fallback to map._container
-    // }).addTo(map);
 }
 function initializingMap() // call this method before you initialize your map.
 {
