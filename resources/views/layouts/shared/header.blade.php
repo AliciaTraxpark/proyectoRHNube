51d87eb8d351969bf8f3d4aa20feb241ea2cc3c7
alicia131 <aliciarodriguez131@gmail.com>
2020-11-24 18:00:10 -0500

    espacio

--- resources/views/layouts/shared/header.blade.php
+++ resources/views/layouts/shared/header.blade.php
@@ -104,7 +104,7 @@ use App\persona;
                     12px" ;></span>&nbsp;  @if (count($istaOrganizacion) == 0) <strong id="strongOrganizacion" style="color:
                     rgb(255, 255, 255)">{{$organizacion->organi_razonSocial}}
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; |</strong>
-                        @else <strong style="color:
+                        @else &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong style="color:
                         rgb(255, 255, 255)"> |</strong>
                          @endif
 
