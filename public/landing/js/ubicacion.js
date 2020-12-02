e87932814cd169ad00c56c37aa6b5c9151026264
gabriela sanchez maza <gaby020313@gmail.com>
2020-12-02 13:09:20 -0500

    arreglando errores

--- public/landing/js/ubicacion.js
+++ public/landing/js/ubicacion.js
@@ -359,7 +359,7 @@ function onMostrarPantallas() {
                                             var horaCompararNow = data[index].minuto[j]["ubicacion"][0].hora_ini;
                                             var resp = checkHora(horaInicioNow, horaFinNow, horaCompararNow);
                                             if (resp) {
-                                                sumaRang = (parseFloat(parseFloat(data[index].minuto[j]["captura"][0].rango) + parseFloat(data[index].minuto[j]["ubicacion"][0].rango)) / 2);
+                                                sumaRang = Math.round(parseFloat(parseFloat(data[index].minuto[j]["captura"][0].rango) + parseFloat(data[index].minuto[j]["ubicacion"][0].rango)) / 2);
                                                 sumaActiv = (parseFloat(parseFloat(data[index].minuto[j]["captura"][0].tiempoA) + parseFloat(data[index].minuto[j]["ubicacion"][0].actividad)) / 2);
                                                 promedio = ((sumaActiv / sumaRang) * 100).toFixed(2);
                                                 sumaRangosTotal += sumaRang;
