4df9781f3894235abb22d8ac75da259938ce3298
alicia131 <aliciarodriguez131@gmail.com>
2020-12-01 20:41:02 -0500

    select empleados dis

--- app/Http/Controllers/dispositivosController.php
+++ app/Http/Controllers/dispositivosController.php
@@ -223,16 +223,59 @@ class dispositivosController extends Controller
         ->where('organi_id','=',session('sesionidorg'))
         ->get()->first();
         $nombreOrga= $organizacion->organi_razonSocial;
-        $empleados = DB::table('empleado as e')
-            ->join('persona as p', 'p.perso_id', '=', 'e.emple_persona')
-            ->select('e.emple_id', 'p.perso_nombre', 'p.perso_apPaterno', 'p.perso_apMaterno')
-           /*  ->where('e.emple_estado', '=', 1)    */
-            ->get();
+
+
             $invitadod = DB::table('invitado')
             ->where('user_Invitado', '=', Auth::user()->id)
             ->where('rol_id', '=', 3)
             ->where('organi_id', '=', session('sesionidorg'))
             ->get()->first();
+            if ($invitadod){
+                if ($invitadod->verTodosEmps == 1) {
+                    $empleados = DB::table('empleado as e')
+            ->join('persona as p', 'p.perso_id', '=', 'e.emple_persona')
+            ->select('e.emple_id', 'p.perso_nombre', 'p.perso_apPaterno', 'p.perso_apMaterno')
+            ->get();
+                } else {
+                    $invitado_empleadoIn=DB::table('invitado_empleado as invem')
+                ->where('invem.idinvitado', '=',  $invitadod->idinvitado)
+                ->where('invem.area_id', '=', null)
+                ->where('invem.emple_id', '!=', null)
+                ->get()->first();
+               if($invitado_empleadoIn!=null){
+
+
+                $empleados = DB::table('empleado as e')
+                ->join('persona as p', 'p.perso_id', '=', 'e.emple_persona')
+                ->select('e.emple_id', 'p.perso_nombre', 'p.perso_apPaterno', 'p.perso_apMaterno')
+                ->join('invitado_empleado as inve', 'e.emple_id', '=', 'inve.emple_id')
+                ->join('invitado as invi', 'inve.idinvitado', '=', 'invi.idinvitado')
+                ->where('invi.estado', '=', 1)
+                ->where('invi.idinvitado', '=', $invitadod->idinvitado)
+                ->get();
+               }
+               else{
+                $empleados = DB::table('empleado as e')
+                ->join('invitado_empleado as inve', 'e.emple_area', '=', 'inve.area_id')
+                ->join('invitado as invi', 'inve.idinvitado', '=', 'invi.idinvitado')
+                ->leftJoin('area as a', 'e.emple_area', '=', 'a.area_id')
+                ->join('persona as p', 'p.perso_id', '=', 'e.emple_persona')
+                ->where('invi.estado', '=', 1)
+                ->where('invi.idinvitado', '=', $invitadod->idinvitado)
+                ->select('e.emple_id', 'p.perso_nombre', 'p.perso_apPaterno', 'p.perso_apMaterno')
+                ->get();
+                
+               }
+                }
+            }
+            else{
+                 $empleados = DB::table('empleado as e')
+            ->join('persona as p', 'p.perso_id', '=', 'e.emple_persona')
+            ->select('e.emple_id', 'p.perso_nombre', 'p.perso_apPaterno', 'p.perso_apMaterno')
+            ->get();
+            }
+
+
             if ($invitadod) {
                 if ($invitadod->rol_id != 1) {
                     if( $invitadod->reporteAsisten==1){
