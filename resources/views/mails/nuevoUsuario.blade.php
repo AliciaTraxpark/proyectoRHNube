3eaea81ed8a53c4d934edff7ea2e70d874423bba
alicia131 <aliciarodriguez131@gmail.com>
2021-01-18 10:45:39 -0500

    img correo

--- resources/views/mails/nuevoUsuario.blade.php
+++ resources/views/mails/nuevoUsuario.blade.php
@@ -311,7 +311,7 @@
                                                                 <td align="center"
                                                                     style="padding:0;Margin:0;padding-top:5px;padding-bottom:15px;font-size:0px">
                                                                     <img class="adapt-img"
-                                                                        src="https://i.ibb.co/CnDQDp1/RH-CUADRADO-NUBE.png"
+                                                                     src="http://rhnube.com.pe/landing/images/NUBE_SOLA.png"
                                                                         alt
                                                                         style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic"
                                                                         width="175"></td>
