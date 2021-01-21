2e35120bcbbe669fc93b145ae685d23dd43444a9
alicia131 <aliciarodriguez131@gmail.com>
2021-01-21 17:15:37 -0500

    modelo de marcaciones

--- app/marcacion_puerta.php
+++ app/marcacion_puerta.php
@@ -11,6 +11,8 @@ class marcacion_puerta extends Model
     protected $primaryKey = 'marcaMov_id';
     protected $fillable = ['marcaMov_id','marcaMov_fecha','marcaMov_emple_id',
     'controladores_idControladores','dispositivos_idDispositivos','organi_id','horarioEmp_id','marcaMov_salida',
-    'marca_latitud','marca_longitud','marcaIdActivi','tipoMarcacionB'];
+    'marca_latitud','marca_longitud','marcaIdActivi',
+    'puntoC_id','centC_id',
+    'tipoMarcacionB'];
     public $timestamps = false;
 }
