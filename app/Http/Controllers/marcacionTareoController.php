3e9ee553950dcb989bce0d9ffc873a6437b63e6b
alicia131 <aliciarodriguez131@gmail.com>
2021-01-21 17:30:12 -0500

    marcacioTController

---
+++ app/Http/Controllers/marcacionTareoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\marcacion_tareo;
+use Illuminate\Http\Request;
+
+class marcacionTareoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\marcacion_tareo  $marcacion_tareo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(marcacion_tareo $marcacion_tareo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\marcacion_tareo  $marcacion_tareo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(marcacion_tareo $marcacion_tareo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\marcacion_tareo  $marcacion_tareo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, marcacion_tareo $marcacion_tareo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\marcacion_tareo  $marcacion_tareo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(marcacion_tareo $marcacion_tareo)
+    {
+        //
+    }
+}
