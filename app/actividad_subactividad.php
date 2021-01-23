76db82792b89be97397b7e6e92c940dbb023d8ed
alicia131 <aliciarodriguez131@gmail.com>
2021-01-23 09:32:24 -0500

    modelos

---
+++ app/actividad_subactividad.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class actividad_subactividad extends Model
+{
+    //
+    protected $table = 'actividad_subactividad';
+    protected $primaryKey = 'idactividad_subactividad';
+    protected $fillable = [
+        'idactividad_subactividad',
+        'Activi_id',
+        'subActividad'
+    ];
+    public $timestamps = false;
+}
