b6e8509fdb8f0d7dcd40eeed1c9d5c0d6604655f
alicia131 <aliciarodriguez131@gmail.com>
2021-02-01 12:37:03 -0500

    no guardar

--- public/landing/js/empleado.js
+++ public/landing/js/empleado.js
@@ -2071,7 +2071,8 @@ function calendario2_ed() {
                 info.event.textColor == "0"
             ) {
                 if (info.event.textColor == "111111") {
-                    bootbox.confirm({
+                    $('#editarConfigHorario').modal('show');
+                   /*  bootbox.confirm({
                         title: "Eliminar horario",
                         message:
                             "¿Desea eliminar: " +
@@ -2115,7 +2116,7 @@ function calendario2_ed() {
                                 });
                             }
                         },
-                    });
+                    }); */
                 } else {
                     bootbox.confirm({
                         title: "Eliminar incidencia",
