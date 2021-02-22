d3055ff6ce8f4da1c5d14d10514d5389d6bbe8be
GabrielaRosmeri <gaby020313@gmail.com>
2021-02-22 10:12:45 -0500

    buscando error

--- public/landing/js/modosEmpleado.js
+++ public/landing/js/modosEmpleado.js
@@ -1769,25 +1769,70 @@ function activarEstadoCRT(idVinculacion) {
             "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
         },
         success: function (data) {
-            if (data == 1) {
-                RefreshTablaEmpleado();
-                $.notifyClose();
-                $.notify({
-                    message: "\nTenemos problemas con el servidor mensajeria.Comunicarse con nosotros",
-                    icon: 'admin/images/warning.svg'
-                }, {
-                    position: 'fixed',
-                    icon_type: 'image',
-                    newest_on_top: true,
-                    delay: 5000,
-                    template: '<div data-notify="container" class="col-xs-8 col-sm-2 text-center alert" style="background-color: #fcf8e3;" role="alert">' +
-                        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-                        '<img data-notify="icon" class="img-circle pull-left" height="20">' +
-                        '<span data-notify="title">{1}</span> ' +
-                        '<span style="color:#8a6d3b;" data-notify="message">{2}</span>' +
-                        '</div>',
-                    spacing: 35
-                });
+            // if (data == 1) {
+            //     RefreshTablaEmpleado();
+            //     $.notifyClose();
+            //     $.notify({
+            //         message: "\nTenemos problemas con el servidor mensajeria.Comunicarse con nosotros",
+            //         icon: 'admin/images/warning.svg'
+            //     }, {
+            //         position: 'fixed',
+            //         icon_type: 'image',
+            //         newest_on_top: true,
+            //         delay: 5000,
+            //         template: '<div data-notify="container" class="col-xs-8 col-sm-2 text-center alert" style="background-color: #fcf8e3;" role="alert">' +
+            //             '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+            //             '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+            //             '<span data-notify="title">{1}</span> ' +
+            //             '<span style="color:#8a6d3b;" data-notify="message">{2}</span>' +
+            //             '</div>',
+            //         spacing: 35
+            //     });
+            // } else {
+            //     RefreshTablaEmpleado();
+            //     $.notifyClose();
+            //     $.notify(
+            //         {
+            //             message: "\nProceso con éxito.",
+            //             icon: "admin/images/checked.svg",
+            //         },
+            //         {
+            //             position: "fixed",
+            //             icon_type: "image",
+            //             newest_on_top: true,
+            //             delay: 5000,
+            //             template:
+            //                 '<div data-notify="container" class="col-xs-8 col-sm-2  text-center alert" style="background-color: #dff0d8;" role="alert">' +
+            //                 '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+            //                 '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+            //                 '<span data-notify="title">{1}</span> ' +
+            //                 '<span style="color:#3c763d;" data-notify="message">{2}</span>' +
+            //                 "</div>",
+            //             spacing: 35,
+            //         }
+            //     );
+            // }
+            if (data.respuesta != undefined) {
+                if (data.respuesta == 1) {
+                    RefreshTablaEmpleado();
+                    $.notifyClose();
+                    $.notify({
+                        message: "\nTenemos problemas con el servidor mensajeria.Comunicarse con nosotros",
+                        icon: 'admin/images/warning.svg'
+                    }, {
+                        position: 'fixed',
+                        icon_type: 'image',
+                        newest_on_top: true,
+                        delay: 5000,
+                        template: '<div data-notify="container" class="col-xs-8 col-sm-2 text-center alert" style="background-color: #fcf8e3;" role="alert">' +
+                            '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                            '<img data-notify="icon" class="img-circle pull-left" height="20">' +
+                            '<span data-notify="title">{1}</span> ' +
+                            '<span style="color:#8a6d3b;" data-notify="message">{2}</span>' +
+                            '</div>',
+                        spacing: 35
+                    });
+                }
             } else {
                 RefreshTablaEmpleado();
                 $.notifyClose();
